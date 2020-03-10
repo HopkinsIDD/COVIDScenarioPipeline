a27f016ca9b13331e2fe3a7890564bbcfbc5e718
chadi <joseph.lemaitre@epfl.ch>
2020-03-10 09:30:55 +0100

    fix

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -65,7 +65,7 @@ def steps_SEIR_nb(p_vec, y0, uid, dt, t_inter, nnodes, popnodes,
                 y[:-1,dest] += mv
                 y[:-1,ori] -= mv
 
-        p_expose =    1 - np.exp(-dt*p_vec[0]*(y[I1]+y[I2]+y[I3])**alpha/popnodes)  # vector
+        p_expose =    1 - np.exp(-dt*p_vec[0]*(y[I1]+y[I2]+y[I3])/popnodes)  # vector
 
 
         for i in range(nnodes):
