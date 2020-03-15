6f020db8ee9e86da0eebd3228b1eeea1e6fe21de
hrmeredith12 <hrmeredith12@gmail.com>
2020-03-14 23:28:24 -0400

    Set start date to Jan 1
    Start date to Jan 1, 2020

--- data/build-model-input.R
+++ data/build-model-input.R
@@ -17,7 +17,7 @@ foldername = '../../data/east-coast/'
 # foldername = '../../data/maryland/'
 
 
-dates <- seq.Date(as.Date("2020/3/1"), as.Date("2020/7/1"), 1)
+dates <- seq.Date(as.Date("2020/1/1"), as.Date("2020/7/1"), 1)
 
 NPI <- as.data.frame(matrix(0, dim(county.status)[1],length(dates)))
 colnames(NPI) <- as.Date(dates)
