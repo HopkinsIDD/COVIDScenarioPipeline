df33de6bd5bbd8ebaee96566eb049161d7cd38d5
chadi <joseph.lemaitre@epfl.ch>
2020-03-18 01:53:53 +0100

    NPI movable

--- data/NPI_Scenario1_None.R
+++ data/NPI_Scenario1_None.R
@@ -2,21 +2,8 @@
 
 library(dplyr)
 
-# East Coast
-county.status <- read.csv('data/east-coast/eastcoast_April1_topAR.csv')
-filterUSPS = c('MD', 'DC', 'VA', 'DE', 'PA', 'NJ')
-foldername = '../../data/east-coast/'
-
 # West coast
-#filterUSPS = ['CA', 'OR', 'WA']
-#foldername = '../../data/west-coast/'
-
-# MD
-# county.status <- read.csv('maryland_April1_topAR.csv')
-# filterUSPS = c('MD')
-# foldername = '../../data/maryland/'
-
-
+county.status <- read.csv(paste(c(foldername,'geodata.csv'))
 dates <- seq.Date(as.Date("2020/1/1"), as.Date("2020/7/1"), 1)
 
 NPI <- as.data.frame(matrix(0, dim(county.status)[1],length(dates)))
