bc6a4d95bdbfc0d7048073f77eddb911f687d3af
Shirlee Wohl <24323246+shwohl@users.noreply.github.com>
2020-03-21 17:50:59 -0400

    add submodule command wrappers

---
+++ gitcommands.bat
@@ -0,0 +1,37 @@
+@ECHO FALSE
+
+:: sclone
+:: function to clone a git repository with submodules
+:: and track the submodule branch specified in .gitmodules
+:sclone
+git clone --recurse-submodules %~1
+git submodule update --remote --recursive
+git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+echo "repository successfully cloned; remember to commit any changes to submodule"
+EXIT /B 0
+
+:: spull
+:: function to pull a git repository with submodules
+:: pull the most recent commit of the specified submodule branch
+:: and track that branch
+:spull
+git pull
+git submodule sync --recursive
+git submodule update --init --remote --recursive
+git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+echo "repository successfully updated; remember to commit any changes to submodule"
+EXIT /B 0
+
+:: spush
+:: function to push changes in a repository with submodules
+:spush
+git push --recurse-submodules=on-demand
+EXIT /B 0
+
+:: sdiff
+:: function to get a list of changes for main repository and all submodules
+:sdiff
+git diff && git submodule foreach 'git diff'
+EXIT /B 0
+
+EXIT /B 0
