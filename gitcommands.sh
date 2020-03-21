3173ae90791c933287d24f3f66af4506ac06c92e
Shirlee Wohl <24323246+shwohl@users.noreply.github.com>
2020-03-21 18:00:59 -0400

    update custom git commands to include git- prefix

--- gitcommands.sh
+++ gitcommands.sh
@@ -3,7 +3,7 @@
 # sclone
 # function to clone a git repository with submodules
 # and track the submodule branch specified in .gitmodules
-function sclone(){
+function git-sclone(){
 	git clone --recurse-submodules $1
 	git submodule update --remote --recursive
 	git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
@@ -14,7 +14,7 @@ function sclone(){
 # function to pull a git repository with submodules
 # pull the most recent commit of the specified submodule branch
 # and track that branch
-function spull(){
+function git-spull(){
 	git pull
 	git submodule sync --recursive
 	git submodule update --init --remote --recursive
@@ -24,12 +24,12 @@ function spull(){
 
 # spush
 # function to push changes in a repository with submodules
-function spush(){
+function git-spush(){
 	git push --recurse-submodules=on-demand
 }
 
 # sdiff
 # function to get a list of changes for main repository and all submodules
-function sdiff(){
+function git-sdiff(){
 	git diff && git submodule foreach 'git diff'
 }
