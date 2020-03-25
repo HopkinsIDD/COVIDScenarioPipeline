0e056faec1e539407ff9989c39189bed453daa92
jkamins7 <jkaminsky@jhu.edu>
2020-03-24 21:00:42 -0400

    Fixed a bug with using non CA data by adding another argument to the yml file for which geoids to keep.  Will update the CA repo to conform

--- R/scripts/hosp_run.R
+++ R/scripts/hosp_run.R
@@ -2,8 +2,24 @@ library(devtools)
 library(covidcommon)
 library(hospitalization)
 library(readr)
+library(dplyr)
+library(tidyr)
+library(magrittr)
+library(hospitalization)
+library(data.table)
 
 set.seed(123456789)
+
+option_list = list(
+  optparse::make_option(c("-c", "--config"), action="store", default='config.yml', type='character', help="path to the config file"),
+  optparse::make_option(c("-d", "--deathrate"), action="store", default='high', type='character', help="name of the death scenario to run"),
+  optparse::make_option(c("-s", "--scenario"), action="store", default='None', type='character', help="name of the intervention to run"),
+  optparse::make_option(c("-j", "--jobs"), action="store", default='8', type='numeric', help="number of cores used")
+)
+opt = optparse::parse_args(optparse::OptionParser(option_list=option_list))
+
+config <- covidcommon::load_config(opt$c)
+
 
 # set parameters for time to hospitalization, time to death, time to discharge
 time_hosp_pars <- as_evaled_expression(config$hospitalization$parameters$time_hosp)
@@ -15,7 +31,7 @@ time_vent_pars <- as_evaled_expression(config$hospitalization$parameters$time_ve
 mean_inc <- as_evaled_expression(config$hospitalization$parameters$mean_inc)
 dur_inf_shape <- as_evaled_expression(config$hospitalization$parameters$inf_shape)
 dur_inf_scale <- as_evaled_expression(config$hospitalization$parameters$inf_scale)
-end_date = config$hospitalization$parameters$end
+end_date = lubridate::ymd(config$hospitalization$parameters$end)
 
 # set death + hospitalization parameters
 p_death <- as_evaled_expression(config$hospitalization$parameters$p_death)
@@ -23,23 +39,18 @@ p_death_rate <- as_evaled_expression(config$hospitalization$parameters$p_death_r
 p_ICU <- as_evaled_expression(config$hospitalization$parameters$p_ICU)
 p_vent <- as_evaled_expression(config$hospitalization$parameters$p_vent)
 
-args <- commandArgs(trailingOnly=TRUE)
-data_filename <- args[1]
-cmd <- args[2]
-ncore = as.numeric(args[3])
-if(length(args) == 0){
-  data_filename <- "model_output/mid-west-coast-AZ-NV_NoNPI"
-  cmd <- "high"
-  ncore <- 1
-}
+data_filename <- paste0("model_output/",config$spatial_setup$setup_name,"_",opt$s)
+# config$hospitalization$paths$output_path
+cmd <- opt$d
+ncore = opt$j
+
 names(p_death) = c('low','med','high')
 
-#TODO (jwills): make this geodata file into a CLI arg
 county_dat <- read.csv(file.path(config$spatial_setup$base_path, config$spatial_setup$geodata))
 county_dat$geoid <- as.character(county_dat$geoid)
 county_dat$new_pop <- county_dat$pop2010
 #county_dat <- make_metrop_labels(county_dat)
-target_geo_ids <- county_dat$geoid[county_dat$stateUSPS=="CA"]
+target_geo_ids <- county_dat$geoid[county_dat$targeted]
 
 cat(paste(data_filename,"\n"))
 res_npi3 <- build_hospdeath_par(p_hosp = p_death[cmd]*10,
