40cf311d22cc77a608266720404de0e0d35ea8d1
jkamins7 <jkaminsky@jhu.edu>
2020-03-25 16:20:44 -0400

    Current (working?) version of ReduceR0.py

--- NPI/ReduceR0.py
+++ NPI/ReduceR0.py
@@ -1,13 +1,20 @@
 import numpy as np
 import pandas as pd
-class NPI(object):
-    def __init__(self, geo_ids, period_start_date, period_end_date):
-        self.period_start_date = period_start_date
-        self.period_end_date = period_end_date
-        self.npi = pd.DataFrame(0.0, index=geo_ids, columns=pd.date_range(period_start_date, period_end_date))
-    def uniform_npi(self, low, high, start_date, end_date=None):
-        for dt in pd.date_range(start_date, end_date if end_date else self.period_end_date):
-            self.npi[dt] = np.random.uniform(low, high, size = len(self.npi))
+
+class R0ReductionNPI(object):
+    def __init__(self, geo_ids, start_date, end_date, intervention_config, full_config):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.geo_ids = geo_ids
+        self.r0_reduction = pd.DataFrame(0.0, index=geo_ids, columns=pd.date_range(start_date, end_date))
+        self.intervention_start_date = intervention_config["start_date"].get()
+        self.intervention_end_date = intervention_config["end_date"].get()
+        self.intervention_value = intervention_config["value"].get() # Hoping this will also get tham as a random distribution
+        self.r0_reduction = pd.DataFrame(0.0, index=geo_ids, columns=pd.date_range(self.start_date, self.end_date))
+        for dt in pd.date_range(self.intervention_start_date(), self.intervention_end_date()):
+            self.r0_reduction[dt] = self.intervention_value()
         return self
+    def r0_reduction(self):
+        return self.r0_reduction
     def get(self):
-        return self.npi
+        return self.r0_reduction # for backwards compatibility
