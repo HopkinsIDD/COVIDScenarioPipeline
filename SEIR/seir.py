38d23de860c2f605678c2a3acd21d4e257a62b80
Josh Wills <jwills@apache.org>
2020-03-27 16:23:50 -0700

    Some minimalist changes to the python code that I think should make the core sim loop faster

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -109,22 +109,22 @@ def steps_SEIR_nb(p_vec, y0, uid, dt, t_inter, nnodes, popnodes, mobility,
             y[E] = y[E] + importation[int(t)]
         for ori in range(nnodes):
             for dest in range(nnodes):
-                for c in range(ncomp - 1):
-                    mv[c] = np.random.binomial(
-                        y[c, ori],
-                        1 - np.exp(-dt * mobility[ori, dest] / popnodes[ori]))
-                y[:-1, dest] += mv
-                y[:-1, ori] -= mv
+                if mobility[ori, dest] > 0.0:
+                    for c in range(ncomp - 1):
+                        mv[c] = np.random.binomial(
+                            y[c, ori],
+                            1 - np.exp(-dt * mobility[ori, dest] / popnodes[ori]))
+                    y[:-1, dest] += mv
+                    y[:-1, ori] -= mv
 
         p_expose = 1 - np.exp(-dt * p_vec[0][it] *
                               (y[I1] + y[I2] + y[I3]) / popnodes)  # vector
 
-        for i in range(nnodes):
-            exposeCases[i] = np.random.binomial(y[S][i], p_expose[i])
-            incidentCases[i] = np.random.binomial(y[E][i], p_infect)
-            incident2Cases[i] = np.random.binomial(y[I1][i], p_recover)
-            incident3Cases[i] = np.random.binomial(y[I2][i], p_recover)
-            recoveredCases[i] = np.random.binomial(y[I3][i], p_recover)
+        exposeCases = np.random.binomial(y[S], p_expose)
+        incidentCases = np.random.binomial(y[E], p_infect)
+        incident2Cases = np.random.binomial(y[I1], p_recover)
+        incident3Cases = np.random.binomial(y[I2], p_recover)
+        recoveredCases = np.random.binomial(y[I3], p_recover)
 
         y[S] += -exposeCases
         y[E] += exposeCases - incidentCases
