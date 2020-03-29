58248d3ee2a8daf549e3a895717f43b9250d35d9
Justin Lessler <justin@jhu.edu>
2020-03-29 00:11:26 -0400

    Added flexible support for padding and put the appropriate code to use it in Rmd

--- R/pkgs/report_generation/R/DataLoadFuncs.R
+++ R/pkgs/report_generation/R/DataLoadFuncs.R
@@ -5,6 +5,9 @@
 ##' @param scenario_dir the subdirectory containing this scenario
 ##' @param post_process function that does processing to sims before loading 
 ##' @param post_process function that does processing after 
+##' @param geoid_len in defined, this we want to make geoids all the same length
+##' @param padding_char character to add to the front of geoids if fixed length
+##' 
 ##' 
 ##' @return a combined data frame of all hospital simulations with filters applied pre merge.
 ##' 
@@ -14,13 +17,22 @@
 ##' @export
 load_scenario_sims_filtered <- function(scenario_dir, 
                                         post_process = function(x) {x},
-                                        pre_process = function(x){x}) {
+                                        pre_process = function(x){x},
+                                        geoid_len = 0,
+                                        padding_char = "0") {
   
   require(tidyverse)
   
   files <- dir(sprintf("model_output/%s", scenario_dir),full.names = TRUE)
   
   rc <- list()
+  
+  
+  if (geoid_len > 0) {
+    padfn <- function(x) {x%>% dplyr::mutate(geoid = str_pad(geoid,width =geoid_len,pad=padding_char))}
+  } else {
+    padfn <- function(x) {x}
+  }
   
   for (i in 1:length(files)) {
     
@@ -31,6 +43,7 @@ load_scenario_sims_filtered <- function(scenario_dir,
                                                   comp=col_character()))  %>%
       pre_process %>%
       pivot_longer(cols=c(-time, -comp), names_to = "geoid", values_to="N") %>% 
+      padfn%>%
       post_process %>%
       mutate(sim_num = i)
     
@@ -38,6 +51,8 @@ load_scenario_sims_filtered <- function(scenario_dir,
   }
   
   rc<- dplyr::bind_rows(rc)
+  
+
   
   return(rc)
 }
@@ -49,6 +64,8 @@ load_scenario_sims_filtered <- function(scenario_dir,
 ##' @param scenario_dir the subdirectory containing this scenario
 ##' @param name_filter function that 
 ##' @param post_process function that does processing after 
+##' @param geoid_len in defined, this we want to make geoids all the same length
+##' @param padding_char character to add to the front of geoids if fixed length
 ##' 
 ##' @return a combined data frame of all hospital simulations with filters applied pre merge.
 ##' 
@@ -58,7 +75,9 @@ load_scenario_sims_filtered <- function(scenario_dir,
 ##'@export
 load_hosp_sims_filtered <- function(scenario_dir,
                                     name_filter = "",
-                                    post_process=function(x) {x}) {
+                                    post_process=function(x) {x},
+                                    geoid_len = 0,
+                                    padding_char = "0") {
   
   require(tidyverse)
   
@@ -68,14 +87,22 @@ load_hosp_sims_filtered <- function(scenario_dir,
   rc <- list()
   
   
+  if (geoid_len > 0) {
+    padfn <- function(x) {x%>% dplyr::mutate(geoid = str_pad(geoid,width=geoid_len,pad=padding_char))}
+  } else {
+    padfn <- function(x) {x}
+  }
+  
   for (i in 1:length(files)) {
     file <- files[i]
     tmp <- readr::read_csv(file, col_types = cols(
       .default = col_double(),
       time=col_date(),
       uid=col_character(),
-      comp=col_character()
+      comp=col_character(),
+      geoid=col_character()
     )) %>% 
+      padfn%>%
       post_process %>%
       mutate(sim_num = i)
     
