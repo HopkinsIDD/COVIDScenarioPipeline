81054cab05e635cbd0bad87ad08a5c3cc463ce40
kkintaro <katkintaro@gmail.com>
2020-04-03 13:36:04 -0400

    Wrote another test and cleaned

--- SEIR/test/test_setup.py
+++ SEIR/test/test_setup.py
@@ -12,37 +12,55 @@ TEST_SETUP_NAME = "minimal_test"
 
 DATA_DIR = os.path.dirname(__file__) + "/data"
 
-def test_SpatialSetup():
-    # good one
-    ss = setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
-                            geodata_file=f"{DATA_DIR}/geodata.csv",
-                            mobility_file=f"{DATA_DIR}/mobility.txt",
-                            popnodes_key="population",
-                            nodenames_key="geoid")
+class TestSpatialSetup:
+    def test_SpatialSetup_success(self):
+        ss = setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                                geodata_file=f"{DATA_DIR}/geodata.csv",
+                                mobility_file=f"{DATA_DIR}/mobility.txt",
+                                popnodes_key="population",
+                                nodenames_key="geoid")
 
-    # mobility_file is not the correct dimensions
-    with pytest.raises(ValueError):
-        setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
-                            geodata_file=f"{DATA_DIR}/geodata.csv",
-                            mobility_file=f"{DATA_DIR}/mobility_small.txt",
-                            popnodes_key="population",
-                            nodenames_key="geoid")
+    def test_bad_popnodes_key_fail(self):
+        # Bad popnodes_key error
+        with pytest.raises(ValueError, match=r'.*popnodes_key.*'):
+            setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                                geodata_file=f"{DATA_DIR}/geodata.csv",
+                                mobility_file=f"{DATA_DIR}/mobility_small.txt",
+                                popnodes_key="wrong",
+                                nodenames_key="geoid")
 
-    # Bad popnodes_key error
-    with pytest.raises(ValueError):
-        setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
-                            geodata_file=f"{DATA_DIR}/geodata.csv",
-                            mobility_file=f"{DATA_DIR}/mobility_small.txt",
-                            popnodes_key="wrong",
-                            nodenames_key="geoid")
+    def test_bad_nodenames_key_fail(self):
+        with pytest.raises(ValueError, match=r'.*nodenames_key.*'):
+            setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                                geodata_file=f"{DATA_DIR}/geodata.csv",
+                                mobility_file=f"{DATA_DIR}/mobility.txt",
+                                popnodes_key="population",
+                                nodenames_key="wrong")
 
-    # Bad nodenames_key
-    with pytest.raises(ValueError):
-        setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
-                            geodata_file=f"{DATA_DIR}/geodata.csv",
-                            mobility_file=f"{DATA_DIR}/mobility_small.txt",
-                            popnodes_key="population",
-                            nodenames_key="wrong")
+    def test_mobility_dimensions_fail(self):
+        with pytest.raises(ValueError, match=r".*mobility.*dimensions.*"):
+            setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                                geodata_file=f"{DATA_DIR}/geodata.csv",
+                                mobility_file=f"{DATA_DIR}/mobility_small.txt",
+                                popnodes_key="population",
+                                nodenames_key="geoid")
+
+    def test_mobility_unsymmetric_fail(self):
+        with pytest.raises(ValueError, match=r".*mobility.*symmetric.*"):
+            setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                                geodata_file=f"{DATA_DIR}/geodata.csv",
+                                mobility_file=f"{DATA_DIR}/mobility_unsymmetric.txt",
+                                popnodes_key="population",
+                                nodenames_key="geoid")
+
+    def test_mobility_too_big_fail(self):
+        with pytest.raises(ValueError, match=r".*mobility.*population.*"):
+            setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                                geodata_file=f"{DATA_DIR}/geodata.csv",
+                                mobility_file=f"{DATA_DIR}/mobility_big.txt",
+                                popnodes_key="population",
+                                nodenames_key="geoid")
+
 
 def test_Setup_set_filter():
     ss = setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
