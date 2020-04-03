8d1a6811093c91a31dd3d4f0f77e6e34eb382bec
kkintaro <katkintaro@gmail.com>
2020-04-03 12:45:31 -0400

    Write a couple pytests

--- SEIR/test/test_setup.py
+++ SEIR/test/test_setup.py
@@ -1,57 +1,68 @@
+import datetime
+import numpy as np
+import os
 import pytest
+import confuse
+
 from SEIR import setup
 
 from ..utils import config
 
 TEST_SETUP_NAME = "minimal_test"
 
+DATA_DIR = os.path.dirname(__file__) + "/data"
+
 def test_SpatialSetup():
+    # good one
     ss = setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
-                            geodata_file="test/data/geodata.csv",
-                            mobility_file="test/data/mobility.txt",
-                            popnodes_key="population")
+                            geodata_file=f"{DATA_DIR}/geodata.csv",
+                            mobility_file=f"{DATA_DIR}/mobility.txt",
+                            popnodes_key="population",
+                            nodenames_key="geoid")
 
     # mobility_file is not the correct dimensions
     with pytest.raises(ValueError):
         setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
-                            geodata_file="test/data/geodata.csv",
-                            mobility_file="test/data/mobility_small.txt",
-                            popnodes_key="population")
+                            geodata_file=f"{DATA_DIR}/geodata.csv",
+                            mobility_file=f"{DATA_DIR}/mobility_small.txt",
+                            popnodes_key="population",
+                            nodenames_key="geoid")
 
     # Bad popnodes_key error
-    with pytest.raises(KeyError):
+    with pytest.raises(ValueError):
         setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
-                            geodata_file="test/data/geodata.csv",
-                            mobility_file="test/data/mobility_small.txt",
-                            popnodes_key="pops")
+                            geodata_file=f"{DATA_DIR}/geodata.csv",
+                            mobility_file=f"{DATA_DIR}/mobility_small.txt",
+                            popnodes_key="wrong",
+                            nodenames_key="geoid")
 
-def test_Setup_buildICfromfilter():
-
-    config.set_file("test/data/config.yml")
-
-    s = setup.Setup(setup_name= "test_None",
-                        spatial_setup=setup.SpatialSetup(
-                            setup_name=TEST_SETUP_NAME,
-                            geodata_file="test/data/geodata.csv",
-                            mobility_file="test/data/mobility.txt",
+    # Bad nodenames_key
+    with pytest.raises(ValueError):
+        setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                            geodata_file=f"{DATA_DIR}/geodata.csv",
+                            mobility_file=f"{DATA_DIR}/mobility_small.txt",
                             popnodes_key="population",
-                        ),
-                        nsim=5,
-                        npi_scenario="None",
-                        npi_config=config["interventions"]["settings"]["None"],
-                        ti=config["start_date"].as_date(),
-                        tf=config["end_date"].as_date(),
+                            nodenames_key="wrong")
+
+def test_Setup_set_filter():
+    ss = setup.SpatialSetup(setup_name=TEST_SETUP_NAME,
+                            geodata_file=f"{DATA_DIR}/geodata.csv",
+                            mobility_file=f"{DATA_DIR}/mobility.txt",
+                            popnodes_key="population",
+                            nodenames_key="geoid")
+
+    s = setup.Setup(setup_name="test_set_filter_name",
+                        spatial_setup=ss,
+                        nsim=1,
+                        npi_scenario="test_set_filter_scenario",
+                        npi_config=confuse.Configuration("test"),
+                        ti=datetime.date(2020, 1, 31),
+                        tf=datetime.date(2020, 12, 31),
                         interactive=True,
-                        write_csv=True,
-                        dt=config["dt"].as_number())
-    y0 = s.test_Setup_buildICfromfilter()
-    # y0[S, :] = self.popnodes - draw
-    # y0[E, :] = (draw / 4).astype(np.int)
-    # y0[I1, :] = (draw / 4).astype(np.int)
-    # y0[I2, :] = (draw / 4).astype(np.int)
-    # y0[I3, :] = (draw / 4).astype(np.int)
-    #TODO write test to make sure they sum to y0
+                        write_csv=False,
+                        dt=0.25)
 
+    # filter has bad dimensions
+    with pytest.raises(ValueError):
+        s.set_filter(np.zeros((1,1)))
 
-def test_parameters_quick_draw():
-    pass
