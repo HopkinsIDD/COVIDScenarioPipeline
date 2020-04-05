f6ea82812a129ed96ad2f531af0a7c6c6d2a2eea
kkintaro <katkintaro@gmail.com>
2020-04-05 18:11:52 -0400

    Merge from dataseed

--- SEIR/test/test_seir.py
+++ SEIR/test/test_seir.py
@@ -1,9 +1,6 @@
-import datetime
 import numpy as np
-import pandas as pd
 import os
 import pytest
-import confuse
 
 from SEIR import setup, seir, NPI
 
@@ -34,15 +31,18 @@ def test_steps_SEIR_no_spread():
     seeding = np.zeros((len(s.t_inter), s.nnodes))
     seeding[:,0] = 1
 
-    mobility_ori, mobility_dest = s.mobility.row, s.mobility.col
-    mobility_prob = 1.0 - np.exp(-s.dt * s.mobility.data / s.popnodes[mobility_ori])
+    mobility_geoid_indices = s.mobility.indices
+    mobility_data_indices = s.mobility.indptr
+    mobility_data = s.mobility.data
 
     npi = NPI.NPIBase.execute(npi_config=s.npi_config, global_config=config, geoids=s.spatset.nodenames)
     npi = npi.get().T
 
     for i in range(100):
         states = seir.steps_SEIR_nb(setup.parameters_quick_draw(s, npi),
-                               seeding, 1234, s.dt, s.t_inter, s.nnodes, s.popnodes,
-                               mobility_ori, mobility_dest, mobility_prob, s.dynfilter)
+                           seeding, 1234, s.dt, s.t_inter, s.nnodes, s.popnodes,
+                           mobility_geoid_indices, mobility_data_indices, mobility_data, s.dynfilter)
 
-        assert(states[seir.cumI][1].max() > 0)
+
+        assert states[seir.cumI][1].max() > 0
+
