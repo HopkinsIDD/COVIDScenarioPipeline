932e4044d1a86102e86723e0045cd06c2730e53d
jkamins7 <jkaminsky@jhu.edu>
2020-04-07 02:00:18 -0400

    Revert "Fixes issue #125 and add a parameter"
    This reverts commit a150d15e5c1301d3940f1c79d8c65fa259cab674.

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -148,23 +148,20 @@ def seeding_draw(s, uid):
 def parameters_quick_draw(s, npi):
     sigma = config["seir"]["parameters"]["sigma"].as_evaled_expression()
     gamma = config["seir"]["parameters"]["gamma"].as_random_distribution()() * n_Icomp
-    alpha = config["seir"]["parameters"]["alpha"].as_random_distribution()()
     R0s = config["seir"]["parameters"]["R0s"].as_random_distribution()()
 
     beta = np.multiply(R0s, gamma) / n_Icomp
 
     beta = np.hstack([beta] * len(s.t_inter))
     gamma = np.hstack([gamma] * len(s.t_inter))
-    alpha = np.hstack([alpha] * len(s.t_inter))
     sigma = np.hstack([sigma] * len(s.t_inter))
 
     beta = np.vstack([beta] * s.nnodes)
     gamma = np.vstack([gamma] * s.nnodes)
-    alpha = np.vstack([alpha] * s.nnodes)
     sigma = np.vstack([sigma] * s.nnodes)
 
     npi.index = pd.to_datetime(npi.index.astype(str))
     npi = npi.resample(str(s.dt * 24) + 'H').ffill()
     beta = np.multiply(beta, np.ones_like(beta) - npi.to_numpy().T)
 
-    return (np.array([beta.T, sigma.T, gamma.T, alpha.T]))
+    return (np.array([beta.T, sigma.T, gamma.T]))
