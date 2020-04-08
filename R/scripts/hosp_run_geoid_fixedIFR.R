e2759382fd1661aa3f43721a1acd43fb664929ff
Kyra Grantz <kyragrantz@gmail.com>
2020-04-08 00:49:40 -0400

    age adjusted hospitalization script with user defined IFR, p_hosp_inf

--- R/scripts/hosp_run_geoid_fixedIFR.R
+++ R/scripts/hosp_run_geoid_fixedIFR.R
@@ -12,7 +12,7 @@ set.seed(123456789)
 
 option_list = list(
   optparse::make_option(c("-c", "--config"), action="store", default=Sys.getenv("CONFIG_PATH"), type='character', help="path to the config file"),
-  optparse::make_option(c("-f", "--factor"), action="store", default='all', type='character', help="name of the scaling factor to run, or 'all' to run all of them"),
+  optparse::make_option(c("-d", "--deathrate"), action="store", default='all', type='character', help="name of the death scenario to run, or 'all' to run all of them"),
   optparse::make_option(c("-s", "--scenario"), action="store", default='all', type='character', help="name of the intervention to run, or 'all' to run all of them"),
   optparse::make_option(c("-j", "--jobs"), action="store", default=detectCores(), type='numeric', help="number of cores used"),
   optparse::make_option(c("-p", "--path"), action="store", default="COVIDScenarioPipeline", type='character', help="path to the COVIDScenarioPipeline directory")
@@ -36,19 +36,26 @@ time_vent_pars <- as_evaled_expression(config$hospitalization$parameters$time_ve
 # set death + hospitalization parameters
 # read in file
 prob_dat <- readr::read_csv(paste(opt$p,"data","geoid-params.csv",sep='/'))
-scl_fac <- as_evaled_expression(config$hospitalization$parameters$scaling_factor)
-names(scl_fac) = config$hospitalization$parameters$scaling_factor_names
+
+p_death <- as_evaled_expression(config$hospitalization$parameters$p_death)
+names(p_death) = config$hospitalization$parameters$p_death_names
+p_hosp_inf <- as_evaled_expression(config$hospitalization$parameters$p_hosp_inf)
+names(p_hosp_inf) = config$hospitalization$parameters$p_death_names
+
+if (length(p_death)!=length(p_hosp_inf)) {
+  stop("Number of IFR and p_hosp_inf values do not match")
+}
 
 config$hospitalization$paths$output_path
-cmd <- opt$f
+cmd <- opt$d
 scenario <- opt$s
 ncore <- opt$j
 
 # Verify that the cmd maps to a known scaling factor value
 if (cmd == "all") {
-   cmd <- names(scl_fac) # Run all of the scaling factor scenarios
- } else if (is.na(scl_fac[cmd]) || is.null(scl_fac[cmd]) || scl_fac[cmd] == 0) {
-   message(paste("Invalid cmd argument:", cmd, "did not match any of the named args in", paste( scl_fac, collapse = ", "), "\n"))
+   cmd <- names(p_death) # Run all of the scaling factor scenarios
+ } else if (is.na(p_death[cmd]) || is.null(p_death[cmd]) || p_death[cmd] == 0) {
+   message(paste("Invalid cmd argument:", cmd, "did not match any of the named args in", paste( p_death, collapse = ", "), "\n"))
    quit("yes", status=1)
  }
  if (scenario == "all" ) {
@@ -69,19 +76,20 @@ for (scn0 in scenario) {
   for (cmd0 in cmd) {
     data_filename <- paste0("model_output/",config$name,"_",scn0)
     cat(paste(data_filename, "\n"))
-    cat(paste("Running hospitalization scenario: ", cmd0, "with scaling factor", scl_fac[cmd0], "\n"))
-    res_npi3 <- build_hospdeath_geoid_par(prob_dat=prob_dat,
-                                          scl_fac = scl_fac[cmd0],
-                                          time_symp_pars=time_symp_pars,
-                                          time_hosp_pars=time_hosp_pars,
-                                          time_death_pars=time_death_pars,
-                                          time_disch_pars=time_disch_pars,
-                                          time_ICU_pars = time_ICU_pars,
-                                          time_vent_pars = time_vent_pars,
-                                          time_ICUdur_pars = time_ICUdur_pars,
-                                          cores = ncore,
-                                          data_filename = data_filename,
-                                          scenario_name = paste(cmd0,"scaling",sep="_")
+    cat(paste("Running hospitalization scenario: ", cmd0, "with IFR", p_death[cmd0], "\n"))
+    res_npi3 <- build_hospdeath_geoid_fixedIFR_par(prob_dat=prob_dat,
+                                                   p_death= p_death[cmd0],
+                                                   p_hosp_inf = p_hosp_inf[cmd0],
+                                                   time_symp_pars=time_symp_pars,
+                                                   time_hosp_pars=time_hosp_pars,
+                                                   time_death_pars=time_death_pars,
+                                                   time_disch_pars=time_disch_pars,
+                                                   time_ICU_pars = time_ICU_pars,
+                                                   time_vent_pars = time_vent_pars,
+                                                   time_ICUdur_pars = time_ICUdur_pars,
+                                                   cores = ncore,
+                                                   data_filename = data_filename,
+                                                   scenario_name = paste(cmd0,"death",sep="_")
     )
   }
 }
