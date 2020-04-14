85e848497dfb818ea6f3d7aeaa815d303ef8d23c
Josh Wills <jwills@apache.org>
2020-04-14 15:17:07 -0700

    Dependencies for arrow in R as well

--- packages.R
+++ packages.R
@@ -35,3 +35,7 @@ install_version("nnls", version = "1.4", repos = repo_url)
 
 install_github("HopkinsIDD/globaltoolboxlite")
 install_github("HopkinsIDD/covidImportation", ref = "7ae00f6c59da3b4f3d4f35316086c6bc6c743fdc")
+
+# Install arrow and it's extra dependencies
+install_version("arrow", version = "0.16.0.2", repos = repo_url)
+arrow::install_arrow()
