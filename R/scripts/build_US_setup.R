d813e2ed9a37ac622bca7ba35565dc5c7c2d586d
jkamins7 <jkaminsky@jhu.edu>
2020-04-18 01:24:37 -0400

    Updated build_US_setup.R to work with the current setup

--- R/scripts/build_US_setup.R
+++ R/scripts/build_US_setup.R
@@ -19,16 +19,31 @@ filterUSPS <- config$spatial_setup$modeled_states
 commute_data <- readr::read_csv(paste(opt$p,"data","united-states-commutes","commute_data.csv",sep='/'))
 census_data <- readr::read_csv(paste(opt$p,"data","united-states-commutes","census_tracts_2010.csv", sep = '/'))
 
+
 census_data <- census_data %>%
   filter(USPS %in% filterUSPS) %>%
   select(USPS,GEOID,POP10) %>%
-  mutate(GEOID = substr(GEOID,1,5)) %>%
+  mutate(GEOID = substr(GEOID,1,5))
+
+name_changer <- setNames(
+  unique(census_data$GEOID),
+  unique(census_data$GEOID)
+)
+name_changer[grepl("^60",name_changer)] <- "60000"
+name_changer[grepl("^66",name_changer)] <- "66000"
+name_changer[grepl("^69",name_changer)] <- "69000"
+name_changer[grepl("^72",name_changer)] <- "72000"
+name_changer[grepl("^78",name_changer)] <- "78000"
+
+census_data <- census_data %>%
+  mutate(GEOID = name_changer[GEOID]) %>%
   group_by(GEOID) %>%
   summarize(USPS = unique(USPS),POP10 = sum(POP10)) %>%
   arrange(POP10)
 
 commute_data <- commute_data %>%
   mutate(OFIPS = substr(OFIPS,1,5), DFIPS = substr(DFIPS,1,5)) %>%
+  mutate(OFIPS = name_changer[OFIPS], DFIPS = name_changer[DFIPS]) %>%
   filter(OFIPS %in% census_data$GEOID, DFIPS %in% census_data$GEOID) %>%
   group_by(OFIPS,DFIPS) %>%
   summarize(FLOW = sum(FLOW)) %>%
@@ -51,10 +66,16 @@ if(opt$w){
   rc <- rc %>%pivot_wider(OFIPS,names_from=DFIPS,values_from=FLOW, values_fill=c("FLOW"=0),values_fn = list(FLOW=sum))
 }
 
-if(!isTRUE(all(rc$OFIPS == census_data$GEOID))){
-  stop("There was a problem generating the mobility matrix")
-}
-
 print(outdir)
-write.csv(file = file.path(outdir,'mobility.txt'), as.matrix(rc[,-1]), row.names=FALSE, col.names = FALSE, sep = " ")
-write.csv(file = file.path(outdir,'geodata.csv'), census_data)
+if(opt$w){
+  if(!isTRUE(all(rc$OFIPS == census_data$GEOID))){
+    stop("There was a problem generating the mobility matrix")
+  }
+  write.table(file = file.path(outdir,'mobility.txt'), as.matrix(rc[,-1]), row.names=FALSE, col.names = FALSE, sep = " ")
+} else {
+  names(rc) <- c("ori","dest","amount")
+  rc <- rc[rc$ori != rc$dest,]
+  write.csv(file = file.path(outdir,'mobility.csv'), rc, row.names=FALSE)
+}
+names(census_data) <- c("geoid","USPS","pop2010")
+write.csv(file = file.path(outdir,'geodata.csv'), census_data,row.names=FALSE)
