a3d4462c032a0b649cdb7c1821a7a7197473ef23
jkamins7 <jkaminsky@jhu.edu>
2020-04-25 12:38:18 -0400

    Changing the name of reprot generation to be the same as it's package name

--- local_install.R
+++ local_install.R
@@ -5,6 +5,4 @@ file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
 pkg.dir <- paste0(dirname(script.name), "/R/pkgs/")
 
-install_local(paste0(pkg.dir, "covidcommon"), force=TRUE, upgrade="always")
-install_local(paste0(pkg.dir, "hospitalization"), force=TRUE, upgrade="always")
-install_local(paste0(pkg.dir, "report_generation"), force=TRUE, upgrade="always")
+install.packages(list.files(pkg.dir,full.names=TRUE),type='source',repos=NULL)
