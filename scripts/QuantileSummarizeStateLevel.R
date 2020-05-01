20feb73869b9b5427287a0226a8c2b34d12b6b55
kkintaro <katkintaro@gmail.com>
2020-04-30 22:52:24 -0400

    Integrate quantile scripts into Makefile

--- scripts/QuantileSummarizeStateLevel.R
+++ scripts/QuantileSummarizeStateLevel.R
@@ -2,16 +2,19 @@
 suppressMessages({
     library(optparse, quietly=TRUE)
     library(tidyverse, quietly=TRUE)
+    library(parallel, quietly=TRUE)
 })
 
 ##List of specified options
 option_list <- list(
-    make_option("--name_filter", type="character", default="", help="filename filter, usually deaths"),
-    make_option("--nfiles", type="numeric", default=NA, help="number of files to load, default is all"),
-    make_option("--ncores", type="numeric", default=6, help="number of cores to use in data load, default =6"),
-    make_option(c("--outfile","-o"), type="character", default=NULL, help="file to saver output"),
-    make_option("--start_date", type="character", default="2020-01-01", help="earliest date to include"),
-    make_option("--end_date",  type="character", default="2022-01-01", help="latest date to include")
+    make_option(c("-c", "--config"), action="store", default=Sys.getenv("CONFIG_PATH"), type='character', help="path to the config file"),
+    make_option(c("-j", "--jobs"), action="store", default=detectCores(), type='numeric', help="number of cores used"),
+    make_option(c("-n", "--num_simulations"), action="store", default=-1, type='numeric', help="number of simulations to run, overrides config file value"),
+
+    make_option(c("-d", "--name_filter"), type="character", default="", help="filename filter, usually deaths"),
+    make_option(c("-o","--outfile"), type="character", default=NULL, help="file to save output"),
+    make_option("--start_date", type="character", default=NULL, help="earliest date to include"),
+    make_option("--end_date",  type="character", default=NULL, help="latest date to include")
 )
 
 opt_parser <- OptionParser(option_list = option_list, usage="%prog [options] [one or more scenarios]")
@@ -25,16 +28,28 @@ if(is.null(opt$outfile)) {
     stop("outfile must be specified")
 }
 
-##Load the geodata file
-suppressMessages(geodata <- readr::read_csv("data/geodata.csv"))
+config <- covidcommon::load_config(opt$c)
+if (length(config) == 0) {
+  stop("no configuration found -- please set CONFIG_PATH environment variable or use the -c command flag")
+}
 
+##Load the geodata file
+suppressMessages(geodata <- readr::read_csv(paste0(config$spatial_setup$base_path,"/",config$spatial_setup$geodata)))
+geodata <- geodata %>% mutate(geoid=as.character(geoid))
 
 ## Register the parallel backend
-doParallel::registerDoParallel(opt$ncores)
+doParallel::registerDoParallel(opt$jobs)
 
 
 ##Convert times to date objects
+if (is.null(opt$start_date)) {
+  opt$start_date <- as.Date(config$start_date)
+}
 opt$start_date <- as.Date(opt$start_date)
+
+if (is.null(opt$end_date)) {
+  opt$end_date <- config$end_date
+}
 opt$end_date <- as.Date(opt$end_date)
 
 
@@ -62,10 +77,12 @@ post_proc <- function(x,geodata,opt) {
 ##Run over scenarios and death rates as appropriate. Note that
 ##Final results will average accross whatever is included
 res_state <-list()
+setup_name <- config$spatial_setup$setup_name
 for (i in 1:length(scenarios)) {
-    res_state[[i]] <- report.generation::load_hosp_sims_filtered(scenarios[i],
+    scenario_dir = paste0(setup_name,"_",scenarios[i])
+    res_state[[i]] <- report.generation::load_hosp_sims_filtered(scenario_dir,
                                                                  name_filter = opt$name_filter,
-                                                                 num_files = opt$nfiles,
+                                                                 num_files = opt$num_simulations,
                                                                  post_process = post_proc,
                                                                  geodata=geodata,
                                                                  opt=opt)%>%
