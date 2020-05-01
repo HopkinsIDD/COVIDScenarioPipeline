45d39cc566a396ca82a74dc3deeee6d0f84c4e2e
Josh Wills <josh.wills@gmail.com>
2020-04-30 23:28:13 -0700

    Fix myriad bugs in this script

--- csv_to_csr.py
+++ csv_to_csr.py
@@ -2,7 +2,7 @@
 
 import click
 import pandas as pd
-import pathlib
+import os
 import scipy.sparse
 
 from SEIR.utils import config
@@ -15,25 +15,25 @@ def convert(config_file):
     config.set_file(config_file)
 
     spatial_config = config["spatial_setup"]
-    spatial_base_path = pathlib.Path(spatial_config["base_path"].get())
+    spatial_base_path = spatial_config["base_path"].get()
 
-    geodata_file = spatial_base_path / spatial_config["geodata"].get(),
-    mobility_file = spatial_base_path / spatial_config["mobility"].get()
-    mobility_npz = spatial_base_path / spatial_config["mobility"].get().replace(".csv", ".npz")
+    geodata_file = os.path.join(spatial_base_path, spatial_config["geodata"].get())
+    mobility_file = os.path.join(spatial_base_path, spatial_config["mobility"].get())
+    mobility_npz = os.path.join(spatial_base_path, spatial_config["mobility"].get().replace(".csv", ".npz"))
 
+    nodenames_key = spatial_config["nodenames"].get()
     data = pd.read_csv(geodata_file, converters={nodenames_key: lambda x: str(x)}) # geoids and populations
-    nnodes = len(self.data)
-    nodenames = data[spatial_config["nodenames"].get()].tolist()
+    nnodes = len(data)
+    nodenames = data[nodenames_key].tolist()
     mobility_data = pd.read_csv(mobility_file, converters={'ori': lambda x: str(x), 'dest': lambda x: str(x)})
 
-    mobility = scipy.sparse.lil_matrix((self.nnodes, self.nnodes))
+    mobility = scipy.sparse.lil_matrix((nnodes, nnodes))
     for index, row in mobility_data.iterrows():
         mobility[nodenames.index(row['ori']), nodenames.index(row['dest'])] = row['amount']
         if (nodenames.index(row['ori']) == nodenames.index(row['dest'])):
                     raise ValueError(f"Mobility fluxes with same origin and destination: '{row['ori']}' to {row['dest']} in long form matrix. This is not supported")
 
-    with open(mobility_npz, "w") as f:
-        scipy.sparse.save_npz(scipy.sparse.csr_matrix(mobility), f)
+    scipy.sparse.save_npz(mobility_npz, scipy.sparse.csr_matrix(mobility))
 
 
 if __name__ == "__main__":
