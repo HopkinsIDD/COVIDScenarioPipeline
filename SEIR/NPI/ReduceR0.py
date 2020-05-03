e0be49c639ed62f87d0729b952e33e7b9302e0b5
jkamins7 <jkaminsky@jhu.edu>
2020-05-03 14:26:10 -0400

    Updated Reduce interventions to be more object oriented

--- SEIR/NPI/ReduceR0.py
+++ SEIR/NPI/ReduceR0.py
@@ -4,8 +4,7 @@ import numpy as np
 from .base import NPIBase
 
 
-# ReduceR0 is redundant with the newer Reduce NPI configured with parameter: 'r0'
-# Kept for backwards compatibility
+
 class ReduceR0(NPIBase):
     def __init__(self, *, npi_config, global_config, geoids, loaded_df = None):
         super().__init__(npi_config)
@@ -14,14 +13,25 @@ class ReduceR0(NPIBase):
         self.end_date = global_config["end_date"].as_date()
 
         self.geoids = geoids
+        
+        self.npi = pd.DataFrame(0.0, index=self.geoids,
+                            columns=pd.date_range(self.start_date, self.end_date))
 
-        # Optional config fields "period_start_date" and "period_end_date"
-        # If unspecified, runs from global start_date to end_date
+        self.intervention_name = npi_config.name
+
         self.period_start_date = npi_config["period_start_date"].as_date() \
             if npi_config["period_start_date"].exists() else self.start_date
         self.period_end_date = npi_config["period_end_date"].as_date() \
             if npi_config["period_end_date"].exists() else self.end_date
 
+        if loaded_df is None:
+            self.createFromConfig(npi_config)
+        else:
+            self.createFromDf(loaded_df)
+
+        self.__checkErrors()
+
+    def __checkErrors(self):
         if not (self.start_date <= self.period_start_date <= self.end_date):
             raise ValueError(f"period_start_date ({self.period_start_date}) is not between global dates [{self.start_date}, {self.end_date}]")
         if not (self.start_date <= self.period_end_date <= self.end_date):
@@ -29,35 +39,40 @@ class ReduceR0(NPIBase):
         if self.period_end_date < self.start_date:
             raise ValueError(f"period_end_date  ({self.period_end_date}) is less than period_start_date ({self.period_start_date})")
 
-        self.dist = npi_config["value"].as_random_distribution()
+        for n in self.affected_geoids:
+            if n not in self.geoids:
+                raise ValueError(f"Invalid config value {n.name} ({node}) not in geoids")
 
+        # Validate
+        if (self.npi == 0).all(axis=None):
+            print(f"Warning: The intervention in config: {self.intervention_name} does nothing.")
+
+        if (self.npi > 1).any(axis=None):
+            raise ValueError(f"The intervention in config: {self.intervention_name} has reduction of {param_name} is greater than 1")
+
+    def createFromConfig(self, npi_config):
         # Optional config field "affected_geoids"
         # If values of "affected_geoids" is "all" or unspecified, run on all geoids.
         # Otherwise, run only on geoids specified.
-        affected = geoids
+        self.affected_geoids = self.geoids
         if "affected_geoids" in npi_config and npi_config["affected_geoids"].get() != "all":
-            affected = []
+            self.affected_geoids = []
             for n in npi_config["affected_geoids"]:
                 node = str(n.get()) # because confuse may read as an int
-                if node not in geoids:
-                    raise ValueError(f"Invalid config value {n.name} ({node}) not in geoids")
-                affected.append(node)
+                self.affected_geoids.append(node)
 
-        if loaded_df is None:
-            self.npi = pd.DataFrame(0.0, index=geoids,
-                                columns=pd.date_range(self.start_date, self.end_date))
-            period_range = pd.date_range(self.period_start_date, self.period_end_date)
-            self.npi.loc[affected, period_range] = np.tile(self.dist(size=len(affected)), (len(period_range), 1)).T
-        else:
-            loaded_df.index = loaded_df.time
-            loaded_df = loaded_df[loaded_df['npi_name'] == self.name]
-            loaded_df.drop(['time', 'parameter', 'npi_name'], inplace = True, axis = 1)
-            self.npi = loaded_df.T
+        self.dist = npi_config["value"].as_random_distribution()
 
-        if (self.npi == 0).all(axis=None):
-            print(f"Warning: The intervention in config: {npi_config.name} does nothing.")
-
-        print("Warning: The ReduceR0 is redundant with the newer Reduce NPI template configured with parameter: r0")
+        # self.reductions = pd.DataFrame(0.0, index = self.affected_geoids)
+        period_range = pd.date_range(self.period_start_date, self.period_end_date)
+        ## This the line that does the work
+        self.npi.loc[self.affected_geoids, period_range] = np.tile(self.dist(size=len(self.affected_geoids)), (len(period_range), 1)).T
+        
+    def createFromDf(self, loaded_df):
+        loaded_df.index = loaded_df.time
+        loaded_df = loaded_df[loaded_df['npi_name'] == self.name]
+        loaded_df.drop(['time', 'parameter', 'npi_name'], inplace = True, axis = 1)
+        self.npi = loaded_df.T
 
     def getReduction(self, param):
         if param == 'r0':
@@ -70,8 +85,3 @@ class ReduceR0(NPIBase):
         df.index.name = "time"
         df = df.reset_index()
         return df
-
-    def setReductionFromFile(self, npi, param, name):
-        if param == 'r0':
-            self.npi = npi
-            self.name = name
