2212a164120eb38e924ef23b16d001774181e2fe
Sam Shah <sam@skipflag.com>
2020-05-07 19:06:53 -0700

    Add Python SEIR module install to batch inference pipeline

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -50,6 +50,14 @@ if [ $local_install_ret -ne 0 ]; then
 	exit 1
 fi
 
+(cd COVIDScenarioPipeline && python setup.py install)
+python_install_ret=$?
+
+if [ $python_install_ret -ne 0 ]; then
+	echo "Error code returned from running `python setup.py install`: $python_install_ret"
+	exit 1
+fi
+
 # Initialize dvc and run the pipeline to re-create the
 # dvc target
 dvc init --no-scm
