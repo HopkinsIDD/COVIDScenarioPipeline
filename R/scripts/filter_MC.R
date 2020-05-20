eaff5db33a5a15bba6599d8d4c99613aedb52c87
Josh Wills <josh.wills@gmail.com>
2020-05-20 13:00:18 -0700

    Define the environment variables we'll eval if a CLI arg is missing inside of filter_MC.R

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -14,11 +14,11 @@ options(warn=1)
 
 option_list = list(
   optparse::make_option(c("-c", "--config"), action="store", default=Sys.getenv("CONFIG_PATH"), type='character', help="path to the config file"),
-  optparse::make_option(c("-s", "--scenarios"), action="store", default='all', type='character', help="name of the intervention to run, or 'all' to run all of them"),
-  optparse::make_option(c("-d", "--deathrates"), action="store", default='all', type='character', help="name of the death scenarios to run, or 'all' to run all of them"),
-  optparse::make_option(c("-j", "--jobs"), action="store", default="8", type='integer', help="Number of jobs to run in parallel"),
-  optparse::make_option(c("-k", "--simulations_per_slot"), action="store", default=NA, type='integer', help = "number of simulations to run for this slot"),
-  optparse::make_option(c("-n", "--number_of_simulations"), action="store", default="1", type='integer', help = "number of slots to run"),
+  optparse::make_option(c("-s", "--scenarios"), action="store", default=Sys.getenv("COVID_SCENARIO", 'all'), type='character', help="name of the intervention to run, or 'all' to run all of them"),
+  optparse::make_option(c("-d", "--deathrates"), action="store", default=Sys.getenv("COVID_DEATHRATE", 'all'), type='character', help="name of the death scenarios to run, or 'all' to run all of them"),
+  optparse::make_option(c("-j", "--jobs"), action="store", default=Sys.getenv("COVID_JOBS", "8"), type='integer', help="Number of jobs to run in parallel"),
+  optparse::make_option(c("-k", "--simulations_per_slot"), action="store", default=Sys.getenv("COVID_SIMS_PER_SLOT", NA), type='integer', help = "number of simulations to run for this slot"),
+  optparse::make_option(c("-n", "--number_of_simulations"), action="store", default=Sys.geten("COVID_NUM_SLOTS", "1"), type='integer', help = "number of slots to run"),
   optparse::make_option(c("-i", "--this_slot"), action="store", default="1", type='integer', help = "id of this slot"),
   optparse::make_option(c("-y", "--python"), action="store", default="python3", type='character', help="path to python executable"),
   optparse::make_option(c("-r", "--rpath"), action="store", default="Rscript", type = 'character', help = "path to R executable"),
