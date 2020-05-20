adfd036a5efa318cd9666ec9a30e60355f13a1b4
kkintaro <katkintaro@gmail.com>
2020-05-20 10:19:37 -0400

    Add manifest.json to inference_pull_from_s3.sh

--- batch/inference_pull_from_s3.sh
+++ batch/inference_pull_from_s3.sh
@@ -9,3 +9,4 @@ RUN_NAME=$1
 
 aws s3 sync s3://idd-inference-runs/$RUN_NAME/final_output $RUN_NAME/final_output
 aws s3 cp s3://idd-inference-runs/$RUN_NAME.tar.gz $RUN_NAME/input.tar.gz
+aws s3 cp s3://idd-inference-runs/$RUN_NAME/manifest.json $RUN_NAME/manifest.json
