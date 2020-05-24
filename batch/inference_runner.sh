d955ba09b55630fcd408ac18b6172cd5339b6d4a
Josh Wills <josh.wills@gmail.com>
2020-05-24 16:53:22 -0700

    error handling logic for inference runs

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -9,6 +9,13 @@ set -x
 # JOB_NAME the name of the job
 # S3_RESULTS_PATH location in S3 to store the results
 
+# Check to see if we should bail on this run because of accumulated errors in other runs
+failure_count=$(aws s3 ls $S3_RESULTS_PATH/failures/ | wc -l)
+if [ $failure_count -gt 10 ]; then
+	echo "Failing run because number of child job failurs is $failure_count"
+	exit 1
+fi
+
 # setup the python environment
 HOME=/home/app
 PYENV_ROOT=$HOME/.pyenv
@@ -41,14 +48,26 @@ if [ -n "$S3_LAST_JOB_OUTPUT" ]; then
 	done
 fi
 
+error_handler() {
+	msg=$1
+	if [ $AWS_BATCH_JOB_ATTEMPT -eq 3]; then
+		echo $JOB_NAME >> errorfile
+		echo $msg >> errorfile
+		aws s3 cp errorfile $S3_RESULTS_PATH/failures/$AWS_BATCH_JOB_ARRAY_INDEX
+		exit 0
+	else
+		echo $1
+		exit 1
+	fi
+}
+
 # Pick up stuff that changed
 # TODO(jwills): maybe move this to like a prep script?
 Rscript COVIDScenarioPipeline/local_install.R
 local_install_ret=$?
 
 if [ $local_install_ret -ne 0 ]; then
-	echo "Error code returned from running local_install.R: $local_install_ret"
-	exit 1
+	error_handler "Error code returned from running local_install.R: $local_install_ret"
 fi
 
 
@@ -56,8 +75,7 @@ fi
 python_install_ret=$?
 
 if [ $python_install_ret -ne 0 ]; then
-	echo "Error code returned from running `python setup.py install`: $python_install_ret"
-	exit 1
+	error_handler "Error code returned from running `python setup.py install`: $python_install_ret"
 fi
 
 echo "State of directory before we start"
@@ -80,8 +98,7 @@ Rscript COVIDScenarioPipeline/R/scripts/full_filter.R -p COVIDScenarioPipeline -
 
 dvc_ret=$?
 if [ $dvc_ret -ne 0 ]; then
-        echo "Error code returned from full_filter.R: $dvc_ret"
-	exit 1
+        error_handler "Error code returned from full_filter.R: $dvc_ret"
 fi
 
 for output in "${DVC_OUTPUTS_ARRAY[@]}"
