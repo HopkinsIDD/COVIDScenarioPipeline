19ca7667ab51a96db979d65c46198f1826deccfe
chadi <joseph.lemaitre@epfl.ch>
2020-05-28 11:03:05 +0200

    Skeleton for file ingestion

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -13,26 +13,32 @@ import pyarrow as pa
 import pandas as pd
 
 
-def run_parallel(config, setup_name, outdir, scenario_seir, scenario_outcomes, nsim = 1, index=1, n_jobs=1):
+def run_delayframe_outcomes(config, setup_name, outdir, scenario_seir, scenario_outcomes, nsim = 1, index=1, n_jobs=1):
     start = time.monotonic()
     sim_ids = np.arange(index, index + nsim)
 
+    parameter_table = None
+    if (config["outcomes"]["param_from_file"].get()):
+        # stuff to put it in parameter table
+        raise('Loading not supported')
+
     if n_jobs == 1:          # run single process for debugging/profiling purposes
         for sim_id in tqdm.tqdm(sim_ids):
-            onerun_Outcomes(sim_id, config, setup_name, outdir, scenario_seir, scenario_outcomes)
+            onerun_delayframe_outcomes(sim_id, config, setup_name, outdir, scenario_seir, scenario_outcomes, parameter_table)
     else:
-        tqdm.contrib.concurrent.process_map(onerun_Outcomes, sim_ids, itertools.repeat(config), 
+        tqdm.contrib.concurrent.process_map(onerun_delayframe_outcomes, sim_ids, itertools.repeat(config), 
                                                                     itertools.repeat(setup_name), 
                                                                     itertools.repeat(outdir), 
                                                                     itertools.repeat(scenario_seir), 
                                                                     itertools.repeat(scenario_outcomes),
+                                                                    itertools.repeat(parameter_table),
                                             max_workers=n_jobs)
 
     print(f"""
 >> {nsim} outcomes simulations completed in {time.monotonic()-start:.1f} seconds
 """)
 
-def onerun_Outcomes(sim_id, config, setup_name, outdir, scenario_seir, scenario_outcomes):
+def onerun_delayframe_outcomes(sim_id, config, setup_name, outdir, scenario_seir, scenario_outcomes, parameter_table):
     sim_id_str = str(sim_id).zfill(9)
     diffI = pd.read_parquet(f'model_output/{setup_name}/{sim_id_str}.seir.parquet')
     diffI = diffI[diffI['comp'] == 'diffI']
@@ -47,13 +53,19 @@ def onerun_Outcomes(sim_id, config, setup_name, outdir, scenario_seir, scenario_
     outcomes = pd.melt(diffI, id_vars='time', value_name = 'incidence', var_name='place')
     config_outcomes = config["outcomes"]["settings"][scenario_outcomes]
     for new_comp in config_outcomes:
-
         if config_outcomes[new_comp]['source'].exists():
             # Read the config for this compartement
             source = config_outcomes[new_comp]['source'].as_str()
             probability = config_outcomes[new_comp]['probability']['value'].as_random_distribution()
             delay = config_outcomes[new_comp]['delay']['value'].as_random_distribution()
-            
+
+            # Overwrite parameters with place specific one:
+            if (parameter_table):
+                if f'D{new_comp}' in parameter_table.keys:
+                    print('changing delay')
+                if f'L{new_comp}|{source}' in parameter_table.keys:
+                    print('Changing probablities')
+    
             # Create new compartement
             all_data[new_comp] = np.empty_like(all_data['incidence'])
             # Draw with from source compartement
@@ -72,6 +84,9 @@ def onerun_Outcomes(sim_id, config, setup_name, outdir, scenario_seir, scenario_
             # Make duration
             if config_outcomes[new_comp]['duration'].exists():
                 duration = config_outcomes[new_comp]['duration']['value'].as_random_distribution()
+                if (parameter_table):
+                    if f'L{new_comp}' in parameter_table.keys:
+                        print('changing durations')
                 all_data[new_comp+'_curr'] = np.cumsum(all_data[new_comp], axis = 0) - shift(np.cumsum(all_data[new_comp], axis=0), int(duration(size=1)))
                 
                 df = pd.DataFrame(all_data[new_comp+'_curr'], columns=places, index=dates)
