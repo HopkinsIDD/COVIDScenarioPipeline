1654e5976a1ae3bc08944d0bd721b506817f947d
shauntruelove <satruelove@gmail.com>
2020-06-09 11:02:38 -0400

    adding print message

--- R/scripts/create_seeding.R
+++ R/scripts/create_seeding.R
@@ -47,6 +47,7 @@ option_list = list(
 
 opt = optparse::parse_args(optparse::OptionParser(option_list=option_list))
 
+print(paste0("Using config file: ", opt$config))
 config <- covidcommon::load_config(opt$config)
 if (length(config) == 0) {
   stop("no configuration found -- please set CONFIG_PATH environment variable or use the -c command flag")
