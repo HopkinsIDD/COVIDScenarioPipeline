e8497ce65e4be2b41f5b28a6d905e1f9c062ae14
jlessler <jlessler@jhsph.edu>
2020-06-13 01:06:24 -0400

    Testing and readibility changes.

--- R/pkgs/inference/R/filter_MC_runner_funcs.R
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -1,7 +1,6 @@
 ###Functions that help with the running of Filter MC. Can take in full confit.
 
 
-
 ##'Function that performs aggregation and calculates likelihood data across all given locations.
 ##'
 ##' @param all_locations all of the locoations to calculate likelihood for
@@ -12,17 +11,17 @@
 ##' @param data_stat the data we are going to compare to aggregated to the right statistic
 ##' @param hosp_file the filename of the hosp file being used (unclear if needed in scope)
 ##' @param hierarchical_stats the hierarchical stats to use
-##' @param defined_priors information on defined priors. 
+##' @param defined_priors information on defined priors.
 ##' @param geodata the geographics data to help with hierarchies
 ##' @param snpi the file with the npi information
 ##' @param hpar data frame of hospitalization parameters
-##' 
+##'
 ##' @return a data frame of likelihood data.
 ##'
 ##' @export
 ##'
 aggregate_and_calc_loc_likelihoods <- function(all_locations,
-                                               sim_hosp,                                              
+                                               sim_hosp,
                                                obs_nodename,
                                                config,
                                                obs,
@@ -46,21 +45,21 @@ aggregate_and_calc_loc_likelihoods <- function(all_locations,
         local_sim_hosp <- dplyr::filter(sim_hosp, !!rlang::sym(obs_nodename) == location) %>%
             dplyr::filter(time %in% unique(obs$date[obs$geoid == location]))
 
-        
+
         sim_stats <- inference::getStats(
                                     local_sim_hosp,
                                     "time",
                                     "sim_var",
                                     stat_list = config$filtering$statistics
                                 )
-        
 
-  
-        
+
+
+
         ## Get observation statistics
         log_likelihood <- list()
         for(var in names(data_stats[[location]])) {
-          
+
             log_likelihood[[var]] <- inference::logLikStat(
                                                     obs = data_stats[[location]][[var]]$data_var,
                                                     sim = sim_stats[[var]]$sim_var,
@@ -69,9 +68,9 @@ aggregate_and_calc_loc_likelihoods <- function(all_locations,
                                                     add_one = config$filtering$statistics[[var]]$add_one
                                                 )
         }
-        
+
         ## Compute log-likelihoods
-        
+
         likelihood_data[[location]] <- dplyr::tibble(
                                                   ll = sum(unlist(log_likelihood)),
                                                   filename = hosp_file,
@@ -79,12 +78,12 @@ aggregate_and_calc_loc_likelihoods <- function(all_locations,
                                               )
         names(likelihood_data)[names(likelihood_data) == 'geoid'] <- obs_nodename
     }
-    
+
     likelihood_data <- likelihood_data %>% do.call(what=rbind)
-    
-    ##Update  liklihood data based on hierarchical_stats 
+
+    ##Update  liklihood data based on hierarchical_stats
     for (stat in names(hierarchical_stats)) {
-        
+
         if (hierarchical_stats[[stat]]$module=="seir") {
             ll_adjs <- inference::calc_hierarchical_likadj(stat=hierarchical_stats[[stat]]$name,
                                                            infer_frame = snpi,
@@ -92,7 +91,7 @@ aggregate_and_calc_loc_likelihoods <- function(all_locations,
                                                            geo_group_column = hierarchical_stats[[stat]]$geo_group_col,
                                                            transform = hierarchical_stats[[stat]]$transform
                                                            )
-            
+
         } else  if (hierarchical_stats[[stat]]$module=="hospitalization") {
 
             ll_adjs <- inference::calc_hierarchical_likadj(stat=hierarchical_stats[[stat]]$name,
@@ -103,24 +102,24 @@ aggregate_and_calc_loc_likelihoods <- function(all_locations,
                                                            stat_col = "value",
                                                            stat_name_col="parameter"
                                                            )
-            
+
         } else {
             stop("unsupported hierarchical stat module")
         }
 
 
-        
-        ##print(stat) 
+
+        ##print(stat)
         ##print(ll_adjs)
         ##print(range(ll_adjs$likadj))
         ##print(ll_adjs%>%filter(is.na(likadj)))
-        
+
         ##probably a more efficient what to do this, but unclear...
         likelihood_data<- left_join(likelihood_data, ll_adjs) %>%
             replace_na(list(likadj=0))%>% ##avoid unmatched location probles
             mutate(ll = ll + likadj) %>%
             select(-likadj)
-            
+
     }
 
 
@@ -134,7 +133,7 @@ aggregate_and_calc_loc_likelihoods <- function(all_locations,
                                                 defined_priors[[prior]]$likelihood$param
                                                 ))%>%
                 select(geoid, likadj)
-            
+
         }  else if (defined_priors[[prior]]$module=="hospitalization") {
 
             ll_adjs <- hpar %>%
@@ -144,22 +143,137 @@ aggregate_and_calc_loc_likelihoods <- function(all_locations,
                                                 defined_priors[[prior]]$likelihood$param
                                                 ))%>%
                 select(geoid, likadj)
-            
+
         } else {
             stop("unsupported prior module")
         }
 
-        ##print(prior) 
+        ##print(prior)
         ##print(ll_adjs)
         ##print(range(ll_adjs$likadj))
-        
+
         ##probably a more efficient what to do this, but unclear...
         likelihood_data<- left_join(likelihood_data, ll_adjs) %>%
             mutate(ll = ll + likadj) %>%
             select(-likadj)
     }
 
-    
+
 
     return(likelihood_data)
 }
+
+
+
+##'
+##' Function that performs the necessary file copies the end of an MCMC iteration of
+##' filter_MC.
+##'
+##'@param current_index the current indec in the run
+##'@param slot what is the current slot numbe
+##'@param block what is the current block
+##'@param run_id what is the id of this run
+##'@param global_local_prefix the prefix to be put on both global and local runs.
+##'@param gf_prefix the prefix for the directory containing the current globally accepted files.
+##'@param global_block_prefix prefix that describes this block.
+##'
+##'@return TRUE if this succeded.
+##'
+##'@export
+##'
+perform_MCMC_step_copies <- function(current_index,
+                                     slot,
+                                     block,
+                                     run_id,
+                                     global_local_prefix,
+                                     gf_prefix,
+                                     global_block_prefix) {
+
+
+    rc <- list()
+
+    if(current_index != 0){
+        rc$hosp_gf <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'hosp','parquet'),
+            covidcommon::create_file_name(run_id,gf_prefix,slot,'hosp','parquet')
+        )
+
+        rc$llik_gf <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'llik','parquet'),
+            covidcommon::create_file_name(run_id,gf_prefix,slot,'llik','parquet')
+        )
+
+        rc$snpi_gf <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'snpi','parquet'),
+            covidcommon::create_file_name(run_id,gf_prefix,slot,'snpi','parquet')
+        )
+
+        rc$spar_gf <-file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'spar','parquet'),
+            covidcommon::create_file_name(run_id,gf_prefix,slot,'spar','parquet')
+        )
+
+        rc$hpar_gf <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'hpar','parquet'),
+            covidcommon::create_file_name(run_id,gf_prefix,slot,'hpar','parquet')
+        )
+
+        rc$hosp_block <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'hosp','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'hosp','parquet')
+        )
+
+
+        rc$llik_block <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'llik','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'llik','parquet')
+        )
+
+        rc$snpi_block <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'snpi','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'snpi','parquet')
+        )
+
+        rc$spar_block <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'spar','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'spar','parquet')
+        )
+
+
+        rc$hpar_block <- file.copy(
+            covidcommon::create_file_name(run_id,global_local_prefix,current_index,'hpar','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'hpar','parquet')
+        )
+    } else {
+        rc$hosp_prevblk <- file.copy(
+            covidcommon::create_file_name(run_id,global_block_prefix,block - 1 ,'hosp','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'hosp','parquet')
+        )
+
+        rc$llik_prevblk <- file.copy(
+            covidcommon::create_file_name(run_id,global_block_prefix,block - 1,'llik','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'llik','parquet')
+        )
+
+        rc$snmp_prvblk <-file.copy(
+            covidcommon::create_file_name(run_id,global_block_prefix,block - 1,'snpi','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'snpi','parquet')
+        )
+
+        rc$spar_prvblk <- file.copy(
+            covidcommon::create_file_name(run_id,global_block_prefix,block - 1,'spar','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'spar','parquet')
+        )
+
+        rc$hpar_prvblk <- file.copy(
+            covidcommon::create_file_name(run_id,global_block_prefix,block - 1,'hpar','parquet'),
+            covidcommon::create_file_name(run_id,global_block_prefix,block,'hpar','parquet')
+        )
+    }
+
+
+    return(rc)
+
+}
+
+
