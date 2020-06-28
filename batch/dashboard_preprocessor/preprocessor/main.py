4becb1a0e7ff6aa13f2d3c6d68533de1b548b14b
Lily Xu <lilyxu@alum.mit.edu>
2020-06-28 10:22:36 -0700

    Add click commands

--- batch/dashboard_preprocessor/preprocessor/main.py
+++ batch/dashboard_preprocessor/preprocessor/main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 import os
 import s3fs
-import logging
+import click
 import datetime
 import pyarrow.parquet as pq
 
@@ -9,24 +11,32 @@ from utils import get_configs, aggregate_by_state, write_to_file
 from parse import init_obj, get_parquet, parse_sim, d3_transform
 from quantiles import calc_quantiles, transform_quantiles
 from geo import stats_for_county_boundaries
+from actuals import get_actuals
 
-def main(bucket: str, base_path: str, geoids: list = []):
-    # bucket: s3 bucket name, e.g., 'idd-dashboard-runs'
-    # base_path: top-level model file directory, e.g., 'USA-20200618T024241'
-    # geoids [optional]: list of geoids to save, default is all geoids
+@click.command()
+@click.option('--bucket', type=str, default='idd-dashboard-runs', 
+    help='s3 bucket where model parquets to be processed are located')
+@click.option('--folder', type=str, default='USA-20200618T024241', # negotiate this
+    help='Top-level model parquet directory folder')
+# @click.option('--scenarios', type=str, default=['scenarioA', 'scenarioB', 'scenarioA', 'scenarioB'], 
+#     help='Scenarios to process into json for visualizing on dashboard')
+def main(bucket: str, folder: str): # , scenarios: list
 
     # CONFIGURATION ---------------
-    logging.info('starting preprocessing for dashboard...') 
+    print('starting preprocessing for dashboard...', datetime.datetime.now()) 
     configs, \
     scenarios, \
     severities, \
     runs, \
+    geoids, \
     dates, \
-    sims = get_configs(bucket, base_path)
+    sims = get_configs(bucket, folder)
+
+    # sims = 4 # TODO: FOR TESTING 
+    geoids = geoidsCA
 
     # PARSE SIMS ---------------
-    parameters = ['incidI', 'incidD']
-    r0_map = {}
+    r0_map = {}     # track associated r0 to join later
     final = init_obj(geoids, scenarios, severities, parameters, dates)
 
     for config in configs:
@@ -34,70 +44,78 @@ def main(bucket: str, base_path: str, geoids: list = []):
             for severity in severities:
                 for run in runs:
                     for sim in range(1, sims + 1):
-                        sim_df, r0 = get_parquet(bucket, base_path, config, \
-                                             scenario, severity, run, str(sim)) 
-
-                        if sim_df is not None:
+                        sim_pq, r0 = get_parquet(bucket, folder, config, \
+                                                 scenario, severity, run, str(sim)) 
+                        if sim_pq is not None:
                             current = '/'.join([scenario, severity, str(sim)])
+                            start = datetime.datetime.now()
                             print('...', current)
-
-                            # track associated r0 to join later
+                            
                             r0_map[current] = r0
-                            parse_sim(sim_df, final, geoids, scenario, \
-                                      severity, parameters, sim)
+                            parse_sim(sim_pq, final, geoids, scenario, \
+                                severity, parameters, sim, len(dates))
+                            print('end', datetime.datetime.now() - start)
+                            print('--------------')
 
-    # AGGREGATE BY STATE ---------------
-    # add state-level sims to final, pass in init obj that just contains states
-    states = list(set([geoid[0:2] for geoid in geoids]))
-    state_dict = init_obj(states, scenarios, severities, parameters, dates)
-    aggregate_by_state(final, state_dict, states)
+    # print('aggregate by state')
+    # start = datetime.datetime.now()
+    # # AGGREGATE BY STATE ---------------
+    # # add state-level sims to final, pass in init obj that just contains states
+    # states = list(set([geoid[0:2] for geoid in geoids]))
+    # state_dict = init_obj(states, scenarios, severities, parameters, dates)
+    # aggregate_by_state(final, state_dict, states)
+    # print('end---', datetime.datetime.now() - start)
 
+    print('transform')
+    start = datetime.datetime.now()
     # TRANSFORM ---------------
-    # transform each sim dict obj into D3-friendly format to make room for quantiles
+    # transform each sim dict obj into D3-friendly format 
     d3_transform(final, r0_map)
+    print('end---', datetime.datetime.now() - start)
 
-    # CALC QUANTILES ---------------
-    calc_quantiles(final)
+    # CALC ACTUALS DATA ---------------
+    # get_actuals()
 
-    # STATS FOR MAP ---------------
-    # build stats for GeoMap Boundaries before quantiles are transformed
-    geo_obj = stats_for_county_boundaries(final)
-    with open('statsForMap.json', 'w') as f:
-        json.dump(geo_obj, f)
+    # JSON DUMP ---------------  # only save a few for testing...
+    # geoids_to_save = [
+    #     '06', '06037', '06075', '06085', '06019', 
+    #     '36', '36005', '36061', '36081'
+    # ]
+    print('write')
+    start = datetime.datetime.now()
+    write_to_file(final, geoidsCA)
+    print('end---', datetime.datetime.now() - start)
 
-    # TRANSFORM QUANTILES ---------------
-    transform_quantiles(final)
-
-    # JSON DUMP ---------------
-    geoids_to_save = [
-        '06', '06037', '06075', '06085', '06019', 
-        '36', '36005', '36061', '36081'
-    ]
-    write_to_file(final, geoids_to_save)
-
-    logging.info('pre-processing for frontend dashboard complete!')
+    print('dashboard pre-processing complete!', datetime.datetime.now())
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-
-    # geoids = geoidsCA + geoidsNY
-    geoids = ['06085', '06019', '36005']
-    bucket = 'idd-dashboard-runs'
-    base_path = 'USA-20200618T024241'
-
-    main(bucket, base_path, geoids)
-
-# FOR TESTING # list out configs to avoid hitting s3 so much
-# configs = ['USA']
-# scenarios = ['pld_inf']
-# severities = ['low', 'high']
-# runs = ['2020.06.18.02:42:40.', '2020.06.18.02:53:08.']
-# dates = dates_from_constants # constants
-# sims = 3 # 300
+    main()
 
 # TODO: test get_parquet or get_configs?? may require mocking s3 call
 # TODO: get_configs.. num_of_sims = int(len(sevs) / len(list(set(sevs))))
-# TODO: write file ... where to?
 # TODO: test_parse_sims and test_d3_transform failing due to inability to 
 # coax vscode into state where it will leg me run debug tests
+# TODO: drop columns you don't need 
+# TODO: add test or assumption stating what data format is required
+
+    # print('calc quantiles')
+    # start = datetime.datetime.now()
+    # # CALC QUANTILES ---------------
+    # calc_quantiles(final)
+    # print('end---', datetime.datetime.now() - start)
+
+    # print('stats for map')
+    # start = datetime.datetime.now()
+    # # STATS FOR MAP ---------------
+    # # build stats for GeoMap Boundaries before quantiles are transformed
+    # geo_obj = stats_for_county_boundaries(final)
+    # # with open('results/statsForMap.json', 'w') as f:
+    # #     json.dump(geo_obj, f)
+    # print('end---', datetime.datetime.now() - start)
+
+    # print('transform quantiles')
+    # start = datetime.datetime.now()
+    # # TRANSFORM QUANTILES ---------------
+    # transform_quantiles(final)
+    # print('end---', datetime.datetime.now() - start)
