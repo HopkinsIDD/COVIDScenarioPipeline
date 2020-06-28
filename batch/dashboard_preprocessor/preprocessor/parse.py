f8daec604f5872f3f0bc1159b7c8e40c38c81c29
Lily Xu <lilyxu@alum.mit.edu>
2020-06-28 10:21:47 -0700

    Parse by slicing into array chunk and geoid map

--- batch/dashboard_preprocessor/preprocessor/parse.py
+++ batch/dashboard_preprocessor/preprocessor/parse.py
@@ -1,13 +1,12 @@
 import os
 import s3fs
-import logging
 import datetime
 import pandas as pd
 import pyarrow.parquet as pq
 
-# from constants import severities, parameters 
+from constants import severities, parameters 
 # pytest
-from preprocessor.constants import severities, parameters
+# from preprocessor.constants import severities, parameters
 
 def init_obj(geoids: list, scenarios: list, severities: list, 
              parameters: list, dates: list) -> dict:
@@ -31,9 +30,9 @@ def init_obj(geoids: list, scenarios: list, severities: list,
                     }
     return final
 
-def get_parquet(bucket: str, base_path: str, config: str, scenario: str, 
+def get_parquet(bucket: str, folder: str, config: str, scenario: str, 
     severity: str, run: str, sim: str):
-    # returns parquet sim file pd.DataFrame and associated r0 float 
+    # returns sim pyarrow.parquet.ParquetDataset and associated r0 float 
     # file path: {prefix}{index}.{run_id}.{file_type}.parquet
     # prefix: {config_name}/{npi_scenario}/{severity_scenario}/{run_id}/global/final/
     # index: sim number + leading zeros
@@ -41,7 +40,7 @@ def get_parquet(bucket: str, base_path: str, config: str, scenario: str,
     #      global/final/000000245.2020.06.18.02:53:08..hosp.parquet
 
     # build parquet filename path key
-    head = base_path + '/model_output/hosp/'
+    head = folder + '/model_output/hosp/'
     prefix = '/'.join([config, scenario, severity, run, 'global/final/'])
     index = '0' * (9 - len(sim)) + sim
     key = head + prefix + index + '.' + run + '.hosp.parquet'
@@ -52,28 +51,32 @@ def get_parquet(bucket: str, base_path: str, config: str, scenario: str,
     r0_path = path.replace('hosp', 'spar')
 
     try:
-        df = pq.ParquetDataset(path, filesystem=s3).read_pandas().to_pandas()
-        spar_df = pq.ParquetDataset(r0_path, filesystem=s3).read_pandas().to_pandas()
-        r0 = round(float(spar_df[spar_df['parameter'] == 'R0']['value']), 2)
-        return (df, r0)
+        pq_dataset = pq.ParquetDataset(path, filesystem=s3) #.read_pandas().to_pandas()
+        r0 = pq.ParquetDataset(r0_path, filesystem=s3).read(columns=['value'])[0][1].as_py() #.read_pandas().to_pandas()
+        return (pq_dataset, round(r0, 2))
 
     except OSError:
         non_path = '/'.join([config, scenario, severity, run, str(sim)])
-        logging.warning('Obj does not exist in bucket %s', non_path)
+        print('Object does not exist in bucket:', non_path)
         return (None, None)
 
-def parse_sim(df, final: dict, geoids: list, scenario: str, 
-              severity: str, parameters: list, sim: str):
-    # reads sim pd.DataFrame and populates data into final Dict Obj 
+def parse_sim(pq_dataset, final: dict, geoids: list, scenario: str, 
+              severity: str, parameters: list, sim: str, date_len: int):
+    # parameter dataset is pyarrow.parquet.ParquetDataset 
+    # function populates data into final Dict Obj 
 
-    # include all geoids if user did not designate specific geoids
-    geoids = df.geoid.unique().tolist() if geoids == [] else geoids
-    cols = ['time', 'geoid'] + parameters
+    # TODO: geoid_map can be a tuple for further optimization ('3505','6023','2022')
+    arrow_table = pq_dataset.read(columns=parameters)
+    # geoid_idx = 0
+    # geoid = geoid_map[geoid_idx]
 
-    for geoid in geoids:
-        for param in parameters:
-            vals = [int(val) for val in df[df.geoid == geoid][param].tolist()]
-            final[geoid][scenario][severity][param]['sims'][sim] = vals
+    for p_idx, parameter in enumerate(parameters):
+        param_chunk = arrow_table[p_idx]
+        
+        for g_idx, geoid in enumerate(geoids):
+            final[geoid][scenario][severity][parameter]['sims'][sim] = \
+                param_chunk[g_idx * date_len: (g_idx + 1) * date_len].to_pylist()
+                # to_pylist() adds 3 seconds to each sim file
 
 def d3_transform(final: dict, r0_map: dict):
     # transforms each nested simulation dict object into d3-friendly format
@@ -112,3 +115,33 @@ def return_r0(r0_map: dict, scenario: str, severity: str, sim: str) -> float:
 
     key = '/'.join([scenario, severity, str(sim)])
     return r0_map[key]
+
+
+            # take chunk associated with it and sim_obj = chunk
+
+        # for idx, val in enumerate(arrow_table[p_idx]):
+        #     sim_obj = final[geoid][scenario][severity][parameter]['sims']
+        #     # if sim not in sim_obj: # TODO: default dict?
+        #     # instantiate [] * date_len
+        #     # idx into and apply  list.idx[0] = val
+        #     #     sim_obj[sim] = [val]
+        #     # else:
+        #     #     sim_obj[sim].append(val)
+
+        #     if idx >= geoid_idx * date_len: # TODO >= or >
+        #         geoid_idx += 1
+        #         geoid = geoid_map[geoid_idx]
+
+    # for row in df.itertuples():
+    #     for parameter in parameters:
+    #         val = getattr(row, parameter)
+    #         sim_obj = final[row.geoid][scenario][severity][parameter]['sims']
+    #         if sim not in sim_obj: # default dict?
+    #             sim_obj[sim] = [val]
+    #         else:
+    #             sim_obj[sim].append(val)
+
+    # for geoid in geoids:
+    #     for param in parameters:
+    #         vals = [int(val) for val in df[df.geoid == geoid][param].tolist()]
+    #         final[geoid][scenario][severity][param]['sims'][sim] = vals
