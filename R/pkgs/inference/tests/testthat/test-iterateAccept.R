16670ffb24ce88d3ac1cf2b16998731e5b6e3f7d
jkamins7 <jkaminsky@jhu.edu>
2020-07-02 13:29:26 -0400

    Abstracted inference setup to a function call.  TODO: Take the python calls out of the package and make them more abstract (pass in a function which creates the file e.g.)

--- R/pkgs/inference/tests/testthat/test-iterateAccept.R
+++ R/pkgs/inference/tests/testthat/test-iterateAccept.R
@@ -1,43 +1,28 @@
 context("iterateAccept")
 
 test_that("iterateAccept accepts better likelihoods",{
-  ll_ref <- data.frame(
-    ll = -1
-  )
-  ll_new <- data.frame(
-    ll = -.99
-  )
-  ll_col <- 'll'
+  ll_ref <- -1
+  ll_new <-  -.99
   N <- 10000
   expect_equal({
-    all(sapply(seq_len(N),function(x){iterateAccept(ll_ref,ll_new,ll_col)}))
+    all(sapply(seq_len(N),function(x){iterateAccept(ll_ref,ll_new)}))
   },TRUE)
 })
 
 test_that("iterateAccept sometimes accepts worse likelihoods",{
-  ll_ref <- data.frame(
-    ll = -1
-  )
-  ll_new <- data.frame(
-    ll = -.99
-  )
-  ll_col <- 'll'
+  ll_ref <-  -1
+  ll_new <- -.99
   N <- 10000
   expect_lt(
-    abs(mean(sapply(seq_len(N),function(x){iterateAccept(ll_new,ll_ref,ll_col)})) - exp(ll_ref$ll - ll_new$ll)),
+    abs(mean(sapply(seq_len(N),function(x){iterateAccept(ll_new,ll_ref)})) - exp(ll_ref - ll_new)),
     4.5e-3
   )
 
-  ll_ref <- data.frame(
-    ll = -1
-  )
-  ll_new <- data.frame(
-    ll = -.9
-  )
-  ll_col <- 'll'
+  ll_ref <- -1
+  ll_new <-  -.9
   N <- 10000
   expect_lt(
-    abs(mean(sapply(seq_len(N),function(x){iterateAccept(ll_new,ll_ref,ll_col)})) - exp(ll_ref$ll - ll_new$ll)),
+    abs(mean(sapply(seq_len(N),function(x){iterateAccept(ll_new,ll_ref)})) - exp(ll_ref - ll_new)),
     1.15e-2
   )
 })
