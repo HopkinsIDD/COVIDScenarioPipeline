aa10ae7af59a236470c34096a7ae57a9d8f93fd1
jkamins7 <jkaminsky@jhu.edu>
2020-07-07 14:58:30 -0400

    Updated testing and fixed a bug

---
+++ R/pkgs/inference/tests/testthat/test-create_filename_list.R
@@ -0,0 +1,39 @@
+context("initial MCMC setup")
+
+test_that("create_filename_list produces a file of each type",{
+  expect_error({
+    create_filename_list("run_id","prefix",1,"type","extension")
+  },NA)
+  expect_equal({
+    gsub(".*[.]","",create_filename_list("run_id","prefix",1,"type","extension")[['type_filename']])
+  },"extension")
+  expect_true({
+    all(grepl(c("run_id","prefix","type","extension"),create_filename_list("run_id","prefix",1,"type","extension")[['type_filename']]))
+  },"extension")
+
+  expect_error({
+    create_filename_list("run_id","prefix",1)
+  },NA)
+
+  expect_error({
+    create_filename_list("run_id","prefix",1,c("a","b","c"),c("csv","parquet","fake"))
+  },NA)
+  expect_equal({
+    names(create_filename_list("run_id","prefix",1,c("a","b","c"),c("csv","parquet","fake")))
+  },c("a_filename","b_filename","c_filename"))
+
+  expect_equal({
+    rc <- create_filename_list("run_id","prefix",1,c("a","b","c"),c("csv","parquet","fake"))
+    rc <- gsub(".*[.]","",rc)
+    rc <- unname(rc)
+    rc
+  },c("csv","parquet","fake"))
+
+  expect_equal({
+    rc <- create_filename_list("run_id","prefix",1,c("a","b","c"),c("csv","parquet","fake"))
+    rc <- gsub("[.][^.]*$","",rc)
+    rc <- gsub(".*[.]","",rc)
+    rc <- unname(rc)
+    rc
+  },c("a","b","c"))
+})
