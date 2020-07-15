7ae7e0e0ac76c74751734deb978a944232a888aa
shauntruelove <satruelove@gmail.com>
2020-07-15 11:42:28 -0400

    update hpar generation code

--- R/scripts/setup_initial_nonUS_data.R
+++ R/scripts/setup_initial_nonUS_data.R
@@ -287,39 +287,97 @@ if(all.equal(sort(colnames(mobility)), sort(geoid_info$ADMIN2))==TRUE){
 # THis only gets run if the output file name/location for the parquet file are specified
 
 if (!is.null(config$outcomes$param_place_file)){
+    
     geoid_params <- readr::read_csv(config$spatial_setup$geoid_params_file)
     
-    rc <- list()
-    names <- c('hosp_inf','icu_hosp','vent_icu','death_inf')
+    ifr_labels <- c("low", "med", "high")
+    ifr_values <- c(.0025, .005, .01)
+    hosp_death_ratio <- ifelse(is.null(config$outcomes$hosp_death_ratio), 7, config$outcomes$hosp_death_ratio)
     
-    for(name in names){
-        p_name <- paste('p',name,sep='_')
-        rr_name <- paste('rr',name,sep='_')
-        rc[[name]]<- geoid_params[,'geoid']
+    for(i in 1:length(ifr_labels)){
+        
+        ifr_lab <- ifr_labels[i]
+        ifr_val <- ifr_values[i]
+        
+        rc <- list()
+        names <- c('hosp_inf','icu_hosp','vent_icu','death_inf')
+        for(name in names){
+            p_name <- paste('p',name,sep='_')
+            rr_name <- paste('rr',name,sep='_')
+            rc[[name]]<- geoid_params[,'geoid']
+            rc[[name]]$parameter <- paste('p',name,sep='_')
+            if(name == 'hosp_inf'){
+                rc[[name]]$value <- geoid_params[[rr_name]] * ifr_val * hosp_death_ratio
+            }
+            if(name == 'death_inf'){
+                rc[[name]]$value <- geoid_params[[rr_name]] * ifr_val
+            }
+            if(name == 'icu_hosp'){
+                rc[[name]]$value <- geoid_params[[p_name]]
+            }
+            if(name == 'vent_icu'){
+                rc[[name]]$value <- geoid_params[[p_name]]
+            }
+        }
+        
+        name <- 'confirmed_inf'
+        rc[[name]] <- geoid_params[,'geoid']
         rc[[name]]$parameter <- paste('p',name,sep='_')
-        if(name == 'hosp_inf'){
-            rc[[name]]$value <- geoid_params[[rr_name]] * .05
-        }
-        if(name == 'death_inf'){
-            rc[[name]]$value <- geoid_params[[rr_name]] * .005
-        }
-        if(name == 'icu_hosp'){
-            rc[[name]]$value <- geoid_params[[p_name]]
-        }
-        if(name == 'vent_icu'){
-            rc[[name]]$value <- geoid_params[[p_name]]
-        }
+        rc[[name]]$value <- ifelse(!is.null(config$spatial_setup$detect_rate), config$spatial_setup$detect_rate, 0.1)
+        
+        print(paste0("Using a detection rate of: ", round(rc[[name]]$value[1] * 100,1), "%"))
+        
+        rc <- do.call(rbind,rc) %>%
+            separate(parameter, into = c("quantity", "outcome", "source"), sep = "_") %>%
+            dplyr::mutate(quantity = recode(quantity, p="probability")) %>%
+            dplyr::mutate(outcome = recode(outcome, 
+                                           hosp = "incidH",
+                                           icu = "incidICU",
+                                           vent = "incidVent",
+                                           death = "incidD",
+                                           confirmed = "incidC")) %>%
+            dplyr::mutate(source = recode(source,
+                                          inf = "incidI",
+                                          hosp = "incidH",
+                                          icu = "incidICU"))
+        
+        arrow::write_parquet(rc, glue::glue("{config$spatial_setup$base_path}/hospitalization_inference_merge_{ifr_lab}.hpar.parquet"))
+        
     }
     
-    name <- 'confirmed_inf'
-    rc[[name]] <- geoid_params[,'geoid']
-    rc[[name]]$parameter <- paste('p',name,sep='_')
-    rc[[name]]$value <- ifelse(!is.null(config$spatial_setup$detect_rate), config$spatial_setup$detect_rate, 0.1)
-    
-    print(paste0("Using a detection rate of: ", round(rc[[name]]$value[1] * 100,1), "%"))
-    
-    rc <- do.call(rbind,rc)
-    ##rc <- dplyr::filter(rc,gsub('...$','',geoid) == '36')
-    arrow::write_parquet(rc, config$outcomes$param_place_file)
+    # geoid_params <- readr::read_csv(config$spatial_setup$geoid_params_file)
+    # 
+    # rc <- list()
+    # names <- c('hosp_inf','icu_hosp','vent_icu','death_inf')
+    # 
+    # for(name in names){
+    #     p_name <- paste('p',name,sep='_')
+    #     rr_name <- paste('rr',name,sep='_')
+    #     rc[[name]]<- geoid_params[,'geoid']
+    #     rc[[name]]$parameter <- paste('p',name,sep='_')
+    #     if(name == 'hosp_inf'){
+    #         rc[[name]]$value <- geoid_params[[rr_name]] * .05
+    #     }
+    #     if(name == 'death_inf'){
+    #         rc[[name]]$value <- geoid_params[[rr_name]] * .005
+    #     }
+    #     if(name == 'icu_hosp'){
+    #         rc[[name]]$value <- geoid_params[[p_name]]
+    #     }
+    #     if(name == 'vent_icu'){
+    #         rc[[name]]$value <- geoid_params[[p_name]]
+    #     }
+    # }
+    # 
+    # name <- 'confirmed_inf'
+    # rc[[name]] <- geoid_params[,'geoid']
+    # rc[[name]]$parameter <- paste('p',name,sep='_')
+    # rc[[name]]$value <- ifelse(!is.null(config$spatial_setup$detect_rate), config$spatial_setup$detect_rate, 0.1)
+    # 
+    # print(paste0("Using a detection rate of: ", round(rc[[name]]$value[1] * 100,1), "%"))
+    # 
+    # rc <- do.call(rbind,rc)
+    # ##rc <- dplyr::filter(rc,gsub('...$','',geoid) == '36')
+    # arrow::write_parquet(rc, config$outcomes$param_place_file)
 }
 
