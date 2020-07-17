3a5c9b09308404d9c5b62c66f18f988a3f9e7879
jkamins7 <jkaminsky@jhu.edu>
2020-07-17 07:13:48 -0400

    Updated data file

--- R/scripts/create_hospitalization_input_from_geoid-params.R
+++ R/scripts/create_hospitalization_input_from_geoid-params.R
@@ -1,14 +1,17 @@
 geoid_params <- readr::read_csv('COVIDScenarioPipeline/sample_data/geoid-params.csv')
+geoid_params <- tidyr::spread(geoid_params,parameter,value)
 rc <- list()
 names <- c('hosp_inf','icu_hosp','vent_icu','death_inf')
 for(name in names){
   p_name <- paste('p',name,sep='_')
   rr_name <- paste('rr',name,sep='_')
   rc[[name]]<- geoid_params[,'geoid']
-  split_name <- strsplit(name,split='_')
+  split_name <- strsplit(name,split='_')[[1]]
   rc[[name]]$source <- split_name[[2]]
   rc[[name]]$outcome <- split_name[[1]]
   rc[[name]]$quantity <- "relative_probabily"
+  print(names(geoid_params))
+  print(rr_name)
   if(name == 'hosp_inf'){
     rc[[name]]$value <- geoid_params[[rr_name]]
   }
@@ -21,14 +24,20 @@ for(name in names){
   if(name == 'vent_icu'){
     rc[[name]]$value <- geoid_params[[rr_name]]
   }
+  if(!("value" %in% names(rc[[name]] ))){
+    print(names(rc[[name]] ))
+    rc[[name]]$value <- NA
+  }
 }
 
 name <- 'confirmed_inf'
 rc[[name]] <- geoid_params[,'geoid']
 rc[[name]]$quantity <- "relative_probability"
-rc[[name]]$source 
+rc[[name]]$source <- "incidI"
+rc[[name]]$outcome <- "incidC"
 rc[[name]]$value <- .1
 
+print(rc)
 rc <- do.call(rbind,rc)
 ##rc <- dplyr::filter(rc,gsub('...$','',geoid) == '36')
 arrow::write_parquet(rc,"hospitalization.hpar.parquet")
