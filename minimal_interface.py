4ea3690242d2f187d23340abb2091b1b5b28dc80
chadi <joseph.lemaitre@epfl.ch>
2020-08-27 17:25:00 +0200

    print if running stoch or determinstic silulation

--- minimal_interface.py
+++ minimal_interface.py
@@ -77,7 +77,6 @@ write_csv = False
 write_parquet = True
 
 
-
 s = setup.Setup(
     setup_name=config["name"].get() + "_" + str(scenario),
     spatial_setup=setup.SpatialSetup(
@@ -108,6 +107,7 @@ s = setup.Setup(
 
 
 print(f"""
+>> Running ***{'STOCHASTIC' if stoch_traj_flag else 'DETERMINISTIC'}*** SEIR and Outcomes modules
 >> Scenario: {scenario}
 >> Starting {s.nsim} model runs beginning from {s.first_sim_index}
 >> Setup *** {s.setup_name} *** from {s.ti} to {s.tf}
