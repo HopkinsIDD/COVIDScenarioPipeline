71a205852f446b267158e99c287327846b862126
chadi <joseph.lemaitre@epfl.ch>
2021-04-22 10:13:43 +0200

    Profiling reveals stacked and reduce makes up for the time, to be checked because no profile overhead for numba stuff

--- minimal_interface.py
+++ minimal_interface.py
@@ -83,6 +83,60 @@ except NameError:
 
 np.random.seed(rng_seed)
 
+import cProfile
+import pstats
+from functools import wraps
+
+
+def profile(output_file=None, sort_by='cumulative', lines_to_print=None, strip_dirs=False):
+    """A time profiler decorator.
+    Inspired by and modified the profile decorator of Giampaolo Rodola:
+    http://code.activestate.com/recipes/577817-profile-decorator/
+    Args:
+        output_file: str or None. Default is None
+            Path of the output file. If only name of the file is given, it's
+            saved in the current directory.
+            If it's None, the name of the decorated function is used.
+        sort_by: str or SortKey enum or tuple/list of str/SortKey enum
+            Sorting criteria for the Stats object.
+            For a list of valid string and SortKey refer to:
+            https://docs.python.org/3/library/profile.html#pstats.Stats.sort_stats
+        lines_to_print: int or None
+            Number of lines to print. Default (None) is for all the lines.
+            This is useful in reducing the size of the printout, especially
+            that sorting by 'cumulative', the time consuming operations
+            are printed toward the top of the file.
+        strip_dirs: bool
+            Whether to remove the leading path info from file names.
+            This is also useful in reducing the size of the printout
+    Returns:
+        Profile of the decorated function
+    """
+
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            _output_file = output_file or func.__name__ + '.prof'
+            pr = cProfile.Profile()
+            pr.enable()
+            retval = func(*args, **kwargs)
+            pr.disable()
+            pr.dump_stats(_output_file)
+
+            #with open(_output_file, 'w') as f:
+            #    ps = pstats.Stats(pr, stream=f)
+            #    if strip_dirs:
+            #        ps.strip_dirs()
+            #    if isinstance(sort_by, (tuple, list)):
+            #        ps.sort_stats(*sort_by)
+            #    else:
+            #        ps.sort_stats(sort_by)
+            #    ps.print_stats(lines_to_print)
+            return retval
+
+        return wrapper
+
+    return inner
 
 
 s = setup.Setup(
@@ -122,13 +176,26 @@ print(f"""
 
 setup_name = s.setup_name
 print(scenario, deathrate, index, run_id, prefix)
-onerun_OUTCOMES_loadID = lambda index: outcomes.onerun_delayframe_outcomes_load_hpar(config,
-                                                                                     run_id, prefix, int(index), # input
-                                                                                     run_id, prefix, int(index), # output
-                                                                                     deathrate, stoch_traj_flag)
-onerun_OUTCOMES = lambda index: outcomes.run_delayframe_outcomes(config,
-                                                                 run_id, prefix, int(index), # input
-                                                                 run_id, prefix, int(index), # output
-                                                                 deathrate, nsim=1, n_jobs=1, stoch_traj_flag = stoch_traj_flag)
-onerun_SEIR_loadID = lambda sim_id2write, s, sim_id2load: seir.onerun_SEIR_loadID(int(sim_id2write), s, int(sim_id2load), stoch_traj_flag)
-onerun_SEIR = lambda sim_id2write, s: seir.onerun_SEIR(int(sim_id2write), s, stoch_traj_flag)
+
+@profile(sort_by='cumulative', strip_dirs=True)
+def onerun_OUTCOMES_loadID(index):
+
+    outcomes.onerun_delayframe_outcomes_load_hpar(config,
+                                                    run_id, prefix, int(index), # input
+                                                    run_id, prefix, int(index), # output
+                                                    deathrate, stoch_traj_flag)
+
+@profile(sort_by='cumulative', strip_dirs=True)                                                   
+def onerun_OUTCOMES(index):
+    outcomes.run_delayframe_outcomes(config,
+                                        run_id, prefix, int(index), # input
+                                        run_id, prefix, int(index), # output
+                                        deathrate, nsim=1, n_jobs=1, stoch_traj_flag = stoch_traj_flag)
+
+@profile(sort_by='cumulative', strip_dirs=True)
+def onerun_SEIR_loadID(sim_id2write, s, sim_id2load):
+    seir.onerun_SEIR_loadID(int(sim_id2write), s, int(sim_id2load), stoch_traj_flag)
+
+@profile(sort_by='cumulative', strip_dirs=True)
+def onerun_SEIR(sim_id2write, s):
+    seir.onerun_SEIR(int(sim_id2write), s, stoch_traj_flag)
