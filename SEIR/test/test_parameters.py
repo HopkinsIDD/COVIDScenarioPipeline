f2f0156557b3636d23ada23aad0bf118bb00d394
chadi <joseph.lemaitre@epfl.ch>
2021-06-04 11:15:42 +0200

    Creation and read-write test for parameters

---
+++ SEIR/test/test_parameters.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+import pytest
+import warnings
+import shutil
+
+
+import pathlib
+import pyarrow as pa
+import pyarrow.parquet as pq
+import filecmp
+
+from SEIR import setup, seir, NPI, file_paths, Parameters
+
+from ..utils import config
+
+DATA_DIR = os.path.dirname(__file__) + "/data"
+
+
+def test_parameters_from_config_plus_read_write():
+    config.set_file(f"{DATA_DIR}/config_compartmental_model_format.yml")
+    lhs = Parameters.Parameters(seir_config = config["seir"])
+    nt_inter = 10
+    nnodes = 5
+    config.set_file(f"{DATA_DIR}/config_compartmental_model_format.yml")
+    p = Parameters.Parameters(seir_config=config["seir"])
+    p_draw = p.parameters_quick_draw(nt_inter=10, nnodes=5)
+    # test shape
+    assert (p_draw.shape == (len(config["seir"]["parameters"].keys()), nt_inter, nnodes))
+
+    p.parameters_write(p_draw=p_draw, fname='test_pwrite')
+
+    rhs = Parameters.Parameters(seir_config=config["seir"])
+    p_load = rhs.parameters_load(fname='test_pwrite', nt_inter=nt_inter, nnodes=nnodes)
+
+    assert ((p_draw == p_load).all())
