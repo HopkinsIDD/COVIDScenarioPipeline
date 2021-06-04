cb37bc40fafbe77a4618262ed7173af31b13a1cd
jkamins7 <jkaminsky@jhu.edu>
2021-06-04 09:41:09 -0400

    Initial tests passing

--- SEIR/test/test_compartments.py
+++ SEIR/test/test_compartments.py
@@ -33,14 +33,14 @@ def test_check_transitions_parquet_creation():
     assert((lhs.compartments == rhs.compartments).all().all())
     # print(lhs.transitions.source)
     # print(rhs.transitions.source)
-    # assert((lhs.transitions == rhs.transitions).all().all())
-    # assert(lhs == rhs)
+    assert((lhs.transitions == rhs.transitions).all().all())
+    assert(lhs == rhs)
 
     temp_compartments_file = f"{DATA_DIR}/parsed_compartment_compartments.test.parquet"
     temp_transitions_file = f"{DATA_DIR}/parsed_compartment_transitions.test.parquet"
     lhs.toFile(compartments_file = temp_compartments_file, transitions_file = temp_transitions_file)
-    # assert(filecmp.cmp(temp_compartments_file, original_compartments_file))
-    # assert(filecmp.cmp(temp_transitions_file, original_transitions_file))
+    assert(filecmp.cmp(temp_compartments_file, original_compartments_file))
+    assert(filecmp.cmp(temp_transitions_file, original_transitions_file))
 
 def test_check_transitions_parquet_loading():
     config.set_file(f"{DATA_DIR}/config_compartmental_model_format.yml")
@@ -54,19 +54,6 @@ def test_check_transitions_parquet_loading():
         transitions_file = temp_transitions_file
     )
 
-    # assert(lhs.parameters == rhs.parameters) ## parameters objects do not have an == operator
     assert((lhs.compartments == rhs.compartments).all().all())
-    assert(lhs.transitions.shape == rhs.transitions.shape)
-    assert((lhs.transitions["source"] == rhs.transitions["source"]).all())
-    assert((lhs.transitions["destination"] == rhs.transitions["destination"]).all())
-    print(lhs.transitions["rate"])
-    print(rhs.transitions["rate"])
-    print(lhs.transitions["rate"] == rhs.transitions["rate"])
-    print(lhs.transitions["rate"][6])
-    print(rhs.transitions["rate"][6])
-    assert((lhs.transitions["rate"] == rhs.transitions["rate"]).all())
-    # assert((lhs.transitions["proportional_to"] == rhs.transitions["proportional_to"]).all())
-    # print(lhs.transitions.source)
-    # print(rhs.transitions.source)
-    # assert((lhs.transitions == rhs.transitions).all().all())
-    # assert(lhs == rhs)
+    assert((lhs.transitions == rhs.transitions).all().all())
+    assert(lhs == rhs)
