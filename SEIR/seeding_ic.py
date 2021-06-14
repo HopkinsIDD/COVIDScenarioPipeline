ea31e280ec9dd25891066018f5b8889476cd52a1
chadi <joseph.lemaitre@epfl.ch>
2021-06-14 13:52:06 +0200

    definitive dataframe format

--- SEIR/seeding_ic.py
+++ SEIR/seeding_ic.py
@@ -90,10 +90,8 @@ class SeedingAndIC:
             fp = file_paths.create_file_name(setup.in_run_id, setup.in_prefix, sim_id + setup.first_sim_index - 1,
                                              self.initial_conditions_config["initial_file_type"], "parquet",
                                              create_directory=False)
-            print(fp)
             ic_df = pq.read_table(fp).to_pandas()
             ic_df = ic_df[(ic_df["date"] == str(setup.ti)) & (ic_df["value_type"] == 'prevalence')]
-            print(ic_df)
             if ic_df.empty:
                 raise ValueError(f"There is no entry for initial time ti in the provided seeding::states_file.")
 
@@ -102,8 +100,6 @@ class SeedingAndIC:
                 ic_df_compartment = ic_df[ic_df['concat_compartment'] == comp_name]
                 for pl_idx, pl in enumerate(setup.spatset.nodenames):
                     if pl in ic_df.columns:
-                        print(ic_df_compartment)
-                        ic_df_compartment.to_csv('df.csv')
                         y0[comp_idx, pl_idx] = float(ic_df_compartment[pl])
                     elif setup.seeding_config["ignore_missing"].get():
                         logging.warning(
