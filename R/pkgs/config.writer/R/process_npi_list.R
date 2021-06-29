f67b91cdab77b585391523f205fc3b5b5b61d89c
juanderone <57634493+juanderone@users.noreply.github.com>
2021-06-29 16:28:52 -0400

    import pipe

--- R/pkgs/config.writer/R/process_npi_list.R
+++ R/pkgs/config.writer/R/process_npi_list.R
@@ -1,3 +1,21 @@
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
+
+##' load_geodata_file
+##' 
 ##' Convenience function to load geodata.csv
 ##'
 ##' @param filename geodata.csv filename
