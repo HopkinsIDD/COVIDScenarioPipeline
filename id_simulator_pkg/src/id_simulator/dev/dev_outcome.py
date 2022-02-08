e3a4578a1454f722329b66bccb3687d4dbb4fd0f
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-08 16:17:06 +0100

    single packaging

---
+++ id_simulator_pkg/src/id_simulator/dev/dev_outcome.py
@@ -0,0 +1,57 @@
+import numpy as np
+import os
+import pytest
+import warnings
+import shutil
+
+import pathlib
+import pyarrow as pa
+import pyarrow.parquet as pq
+import filecmp
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime
+
+import pytest
+
+from Outcomes import outcomes
+from SEIR.utils import config
+
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import glob, os, sys
+from pathlib import Path
+
+# import seaborn as sns
+import pyarrow.parquet as pq
+import click
+import pyarrow as pa
+from SEIR import file_paths
+
+config.clear()
+config.read(user=False)
+config.set_file("config.yml")
+
+run_id = 333
+index = 1
+deathrate = "high_death_rate"
+prefix = ""
+stoch_traj_flag = True
+
+outcomes.run_delayframe_outcomes(
+    config,
+    int(index),
+    run_id,
+    prefix,  # input
+    int(index),
+    run_id,
+    prefix,  # output
+    deathrate,
+    nsim=1,
+    n_jobs=1,
+    stoch_traj_flag=stoch_traj_flag,
+)
