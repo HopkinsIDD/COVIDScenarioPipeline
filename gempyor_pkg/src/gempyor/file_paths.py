7ca67c4e3fbe7a24de0dc953d7ee4da4e24b00c8
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-16 10:48:35 +0100

    type is a built-in python construct, changing to ftype

--- gempyor_pkg/src/gempyor/file_paths.py
+++ gempyor_pkg/src/gempyor/file_paths.py
@@ -2,34 +2,33 @@ import os
 import datetime
 
 
-def create_file_name(run_id, prefix, index, type, extension, create_directory=True):
+def create_file_name(run_id, prefix, index, ftype, extension, create_directory=True):
     if create_directory:
         os.makedirs(create_dir_name(run_id, prefix, type), exist_ok=True)
     return "model_output/%s/%s%09d.%s.%s.%s" % (
-        type,
+        ftype,
         prefix,
         index,
         run_id,
-        type,
+        ftype,
         extension,
     )
 
-
 def create_file_name_without_extension(
-    run_id, prefix, index, type, create_directory=True
+    run_id, prefix, index, ftype, create_directory=True
 ):
     if create_directory:
-        os.makedirs(create_dir_name(run_id, prefix, type), exist_ok=True)
-    return "model_output/%s/%s%09d.%s.%s" % (type, prefix, index, run_id, type)
+        os.makedirs(create_dir_name(run_id, prefix, ftype), exist_ok=True)
+    return "model_output/%s/%s%09d.%s.%s" % (ftype, prefix, index, run_id, ftype)
 
 
 def run_id():
     return datetime.datetime.strftime(datetime.datetime.now(), "%Y.%m.%d.%H:%M:%S.%Z")
 
 
-def create_dir_name(run_id, prefix, type):
+def create_dir_name(run_id, prefix, ftype):
     return os.path.dirname(
         create_file_name_without_extension(
-            run_id, prefix, 1, type, create_directory=False
+            run_id, prefix, 1, ftype, create_directory=False
         )
     )
