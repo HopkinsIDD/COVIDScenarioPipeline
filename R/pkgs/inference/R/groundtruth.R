4d80c75e473f80500df8dcb1ebec2d2600a4dae5
jkamins7 <jkaminsky@jhu.edu>
2022-02-28 09:33:34 -0500

    Revert "ground truth updates"
    This reverts commit 0b0b3d6c3341e591f5b1878a694ebce99461925a.

--- R/pkgs/inference/R/groundtruth.R
+++ R/pkgs/inference/R/groundtruth.R
@@ -1,108 +1,67 @@
 #' Function to load US COVID data from JHUCSSE
-#'
 #' @param data_path Path where to write the data
 #' @param cache logical indicating whether to cache the data (default = TRUE)
 #' @param gt_source string indicating source of ground truth data. options include "csse" or "usafacts" (default csse)
 #' @param gt_scale string indicating whether "US county" or "US state"-level data
-#' @param variant_filename 
 #'
 #' @return NULL
 #'
 #' @export
-get_ground_truth_file <- function(data_path, cache = TRUE, 
-                                  gt_source = "csse", 
-                                  gt_scale = "US county", 
-                                  gt_source_file = NULL,
-                                  targets = c("Confirmed", "Deaths", "incidI", "incidDeath"), 
-                                  fix_negatives=TRUE, 
-                                  variant_filename = "data/variant/variant_props_long.csv"){
-    
-    data_dir <- dirname(data_path)
-    if(!dir.exists(data_dir)){
-        suppressWarnings(dir.create(data_dir,recursive=TRUE))
-    }
-    if(!(file.exists(data_path) & cache)){
-        message(paste("*** Loading Data from", gt_source, "\n"))
-        cases_deaths <- suppressMessages(
-            covidcommon::get_groundtruth_from_source(source = gt_source, scale = gt_scale, source_file = gt_source_file,
-                                                     variables = targets,
-                                                     incl_unass = ifelse(gt_scale == "US state", TRUE, FALSE), 
-                                                     fix_negatives = fix_negatives,
-                                                     adjust_for_variant = !is.null(variant_filename), 
-                                                     variant_props_file = variant_filename))
-        cases_deaths  <- dplyr::arrange(
-            dplyr::rename(
-                dplyr::mutate(
-                    cases_deaths,
-                    Update = lubridate::ymd(Update)
-                ),
-                date = Update,
-            ),
-            date,
-            FIPS
-        )
-        readr::write_csv(cases_deaths, data_path)
-        rm(cases_deaths)
-        message("*** DONE Loading Data \n")
-    } else {
-        message("*** USING CACHED Data\n")
-    }
-    
-    return()
+get_ground_truth_file <- function(data_path, cache = TRUE, gt_source = "csse", gt_scale = "US county", variant_filename = "data/variant/variant_props_long.csv") {
+  data_dir <- dirname(data_path)
+  if(!dir.exists(data_dir)){
+    suppressWarnings(dir.create(data_dir,recursive=TRUE))
+  }
+  if(!(file.exists(data_path) & cache)){
+    message(paste("*** Loading Data from", gt_source, "\n"))
+    cases_deaths <- suppressMessages(covidcommon::get_groundtruth_from_source(source = gt_source, scale = gt_scale, variables = c("Confirmed", "Deaths", "incidI", "incidDeath"), incl_unass = ifelse(gt_scale == "US state", TRUE, FALSE), adjust_for_variant = !is.null(variant_filename), variant_props_file = variant_filename))
+    cases_deaths  <- dplyr::arrange(
+      dplyr::rename(
+        dplyr::mutate(
+          cases_deaths,
+          Update = lubridate::ymd(Update)
+        ),
+        date = Update,
+      ),
+      date,
+      FIPS
+    )
+    readr::write_csv(cases_deaths, data_path)
+    rm(cases_deaths)
+    message("*** DONE Loading Data \n")
+  } else {
+    message("*** USING CACHED Data\n")
+  }
+
+  return()
 }
 
-
-
-
 #' Function to load US COVID data from USAfacts
-#'
 #' @param data_path Path where to write the data
-#' @param fips_codes 
-#' @param fips_column_name 
-#' @param start_date 
-#' @param end_date 
-#' @param cache 
-#' @param gt_source 
-#' @param gt_scale 
-#' @param variant_filename 
 #'
 #' @export
-get_ground_truth <- function(data_path, 
-                             fips_codes, 
-                             fips_column_name, 
-                             start_date, 
-                             end_date, 
-                             cache = TRUE, 
-                             gt_source = "csse", 
-                             gt_scale = "US county", 
-                             targets = c("Confirmed", "Deaths", "incidI", "incidDeath"), 
-                             fix_negatives = TRUE,
-                             variant_filename = "data/variant/variant_props_long.csv"){
-    
-    get_ground_truth_file(data_path = data_path, cache = cache, 
-                          gt_source = gt_source, gt_scale = gt_scale, 
-                          targets = targets, 
-                          fix_negatives = fix_negatives,
-                          variant_filename = variant_filename)
-    
-    rc <- suppressMessages(readr::read_csv(
-        data_path,
-        col_types = list(FIPS = readr::col_character()),
-    ))
-    rc <- dplyr::filter(
-        rc,
-        FIPS %in% fips_codes,
-        date >= start_date,
-        date <= end_date
+get_ground_truth <- function(data_path, fips_codes, fips_column_name, start_date, end_date, cache = TRUE, gt_source = "csse", gt_scale = "US county", variant_filename = "data/variant/variant_props_long.csv"){
+
+  get_ground_truth_file(data_path = data_path, cache = cache, gt_source = gt_source, gt_scale = gt_scale, variant_filename = variant_filename)
+
+  rc <- suppressMessages(readr::read_csv(
+    data_path,
+    col_types = list(FIPS = readr::col_character()),
+  ))
+  rc <- dplyr::filter(
+    rc,
+    FIPS %in% fips_codes,
+    date >= start_date,
+    date <= end_date
+  )
+  rc <- dplyr::right_join(
+    rc,
+    tidyr::expand_grid(
+      FIPS = unique(rc$FIPS),
+      date = unique(rc$date)
     )
-    rc <- dplyr::right_join(
-        rc,
-        tidyr::expand_grid(
-            FIPS = unique(rc$FIPS),
-            date = unique(rc$date)
-        )
-    )
-    rc <- dplyr::mutate_if(rc, is.numeric, dplyr::coalesce, 0)
-    names(rc)[names(rc) == "FIPS"] <- fips_column_name
-    return(rc)
+  )
+  rc <- dplyr::mutate_if(rc, is.numeric, dplyr::coalesce, 0)
+  names(rc)[names(rc) == "FIPS"] <- fips_column_name
+  return(rc)
 }
