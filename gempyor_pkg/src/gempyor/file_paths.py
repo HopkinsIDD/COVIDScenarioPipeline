23adddbc67553b3995c5dd02bd31d308155e6564
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-03 18:26:19 +0100

    simplifed fn pipeline

--- gempyor_pkg/src/gempyor/file_paths.py
+++ gempyor_pkg/src/gempyor/file_paths.py
@@ -5,14 +5,11 @@ import datetime
 def create_file_name(run_id, prefix, index, ftype, extension, create_directory=True):
     if create_directory:
         os.makedirs(create_dir_name(run_id, prefix, ftype), exist_ok=True)
-    return "model_output/%s/%s%09d.%s.%s.%s" % (
-        ftype,
-        prefix,
-        index,
-        run_id,
-        ftype,
-        extension,
+
+    fn_no_ext = create_file_name_without_extension(
+        run_id, prefix, index, ftype, create_directory=create_directory
     )
+    return f"{fn_no_ext}.%s" % (extension,)
 
 
 def create_file_name_without_extension(
