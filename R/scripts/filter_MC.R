c7f7554971e7d6a84d9263b96427ea45b04e2dde
Shaun Truelove <satruelove@gmail.com>
2022-03-07 15:26:56 -0500

    fix seeding error

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -210,53 +210,50 @@ if (!("gt_end_date" %in% colnames(gt_info))){
 
 gt_sources <- unique(gt_info$gt_source)
 gt_targets_all <- unique(gsub("_(.*)", "", gt_info$data_var))
+variant_props_file <- config$seeding$variant_filename
 
+
+print(paste0("Using variant file: ",variant_props_file, "."))
 print(gt_info)
 
+
+
 obs <- tibble::tibble(geoid = fips_codes_)
-if (length(gt_sources)>1 | length(unique(gt_info$gt_start_date))>1 | length(unique(gt_info$gt_end_date))>1){
+# if (length(gt_sources)>1 | length(unique(gt_info$gt_start_date))>1 | length(unique(gt_info$gt_end_date))>1){
+
+if(!(file.exists(data_path) & opt$cache_gt)){
     
-    if(!(file.exists(data_path) & opt$cache_gt)){
-        for (g in 1:length(gt_sources)){
-            
-            # ground truth targets to pull
-            gt_tmp <- gt_info %>% dplyr::filter(gt_source == gt_sources[g])
-            print(gt_tmp)
-            gt_targets <- unique(gsub("_(.*)", "", gt_tmp$data_var))
-            if (("incidDeath" %in% gt_targets) & !("incidI" %in% gt_targets_all)) gt_targets <- c(gt_targets, "incidI")
-            if (("incidI" %in% gt_targets) & !("incidI" %in% gt_targets_all)) gt_targets <- c(gt_targets, "incidDeath")
-            if ("incidDeath" %in% gt_targets) gt_targets <- c(gt_targets, "Deaths")
-            if ("incidI" %in% gt_targets) gt_targets <- c(gt_targets, "Confirmed")
-            # if ("incidH" %in% gt_targets) gt_targets <- c(gt_targets, "Hospitalizations")
-            
-            obs_ <- inference::get_ground_truth(
-                data_path = data_path,
-                fips_codes = fips_codes_,
-                fips_column_name = obs_nodename,
-                start_date = gt_start_date_,
-                end_date = gt_end_date_,
-                cache = FALSE, # cache later
-                gt_source = gt_sources[g],
-                gt_scale = gt_scale,
-                targets = gt_targets, 
-                fix_negatives = opt$fix_negatives,
-                variant_filename = NULL
-            )    
-            obs <- obs %>% dplyr::full_join(obs_)
-        }
-        # save merged
-        readr::write_csv(obs, data_path)
+    for (g in 1:length(gt_sources)){
         
-    } else {
-        message("*** USING CACHED Data\n")
-        obs <- suppressMessages(readr::read_csv(
-            data_path,
-            col_types = list(geoid = readr::col_character()),
-        ))
+        # ground truth targets to pull
+        gt_tmp <- gt_info %>% dplyr::filter(gt_source == gt_sources[g])
+        print(paste0("Pulling new data from ", gt_sources[g]))
+        print(gt_tmp)
+        gt_targets <- unique(gsub("_(.*)", "", gt_tmp$data_var))
+        if (("incidDeath" %in% gt_targets) & !("incidI" %in% gt_targets_all)) gt_targets <- c(gt_targets, "incidI")
+        if (("incidI" %in% gt_targets) & !("incidI" %in% gt_targets_all)) gt_targets <- c(gt_targets, "incidDeath")
+        if ("incidDeath" %in% gt_targets) gt_targets <- c(gt_targets, "Deaths")
+        if ("incidI" %in% gt_targets) gt_targets <- c(gt_targets, "Confirmed")
+        # if ("incidH" %in% gt_targets) gt_targets <- c(gt_targets, "Hospitalizations")
+        
+        obs_ <- inference::get_ground_truth(
+            data_path = data_path,
+            fips_codes = fips_codes_,
+            fips_column_name = obs_nodename,
+            start_date = gt_start_date_,
+            end_date = gt_end_date_,
+            cache = FALSE, # cache later
+            gt_source = gt_sources[g],
+            gt_scale = gt_scale,
+            targets = gt_targets, 
+            fix_negatives = opt$fix_negatives,
+            variant_filename = NULL
+        )    
+        obs <- obs %>% dplyr::full_join(obs_)
     }
     
     # do variant adjustment
-    if (adjust_for_variant & any(c("incidI", "Confirmed") %in% variables)) {
+    if (!is.null(variant_props_file) & any(c("incidI", "Confirmed") %in% gt_targets_all)) {
         tryCatch({
             obs <- do_variant_adjustment2(obs, variant_props_file, var_targets = c("incidI","Confirmed"))
         }, error = function(e) {
@@ -271,34 +268,51 @@ if (length(gt_sources)>1 | length(unique(gt_info$gt_start_date))>1 | length(uniq
                                            data_var = gsub("incidH", "Hospitalizations", data_var), 
                                            data_var = gsub("incidDeath", "Deaths", data_var)))
     target_ <- gt_infofull$data_var
-    
     for (s in 1:nrow(gt_infofull)){
         na_inds <- !(obs$date >= gt_infofull$gt_start_date[s]) & (obs$date <= gt_infofull$gt_end_date[s])
         obs[na_inds, target_[s]] <- NA
     }
     
-} else {
+    # save merged
+    readr::write_csv(obs, data_path)
     
-    obs <- inference::get_ground_truth(
-        data_path = data_path,
-        fips_codes = fips_codes_,
-        fips_column_name = obs_nodename,
-        start_date = gt_start_date_,
-        end_date = gt_end_date_,
-        cache = opt$cache_gt,
-        gt_source = gt_sources,
-        gt_scale = gt_scale,
-        targets = gt_targets_all, 
-        fix_negatives = opt$fix_negatives,
-        variant_filename = config$seeding$variant_filename
-    )    
+    
+} else {
+    message("*** USING CACHED Data\n")
+    obs <- suppressMessages(readr::read_csv(
+        data_path,
+        col_types = list(geoid = readr::col_character()),
+    ))
 }
 
 
+
+
+
+
+# } else {
+#     
+#     obs <- inference::get_ground_truth(
+#         data_path = data_path,
+#         fips_codes = fips_codes_,
+#         fips_column_name = obs_nodename,
+#         start_date = gt_start_date_,
+#         end_date = gt_end_date_,
+#         cache = opt$cache_gt,
+#         gt_source = gt_sources,
+#         gt_scale = gt_scale,
+#         targets = gt_targets_all, 
+#         fix_negatives = opt$fix_negatives,
+#         variant_filename = config$seeding$variant_filename
+#     )    
+# }
+
 geonames <- unique(obs[[obs_nodename]])
 
+print("Successfully pulled ground truth.")
 
-print("Successfully pull ground truth.")
+
+
 
 
 # Compute Statistics ------------------------------------------------------
