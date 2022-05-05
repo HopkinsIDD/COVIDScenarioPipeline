826d0cc0ec0716a566bb640619631181e62bdb65
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-05-05 12:16:36 +0200

    all gempyor test passes

--- gempyor_pkg/tests/npi/test_npis.py
+++ gempyor_pkg/tests/npi/test_npis.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
-import glob, os, sys
+import glob, os, sys, shutil
 from pathlib import Path
 
 # import seaborn as sns
@@ -41,15 +41,22 @@ def test_full_npis_read_write():
         deathrate="med",
         scenario="inference",
         stoch_traj_flag=False,
-        out_run_id=106,
+        out_run_id=105,
     )
+    #inference_simulator.one_simulation(sim_id2write=1,load_ID=False)
+
+    #outcomes.onerun_delayframe_outcomes(
+    #    sim_id2write=1, s=inference_simulator.s, load_ID=False, sim_id2load=1
+    #)
 
     npi_outcomes = outcomes.build_npi_Outcomes(
         inference_simulator.s, load_ID=False, sim_id2load=None, config=config
     )
-    npi_seir = seir.build_npi_SEIR(
-        inference_simulator.s, load_ID=False, sim_id2load=None, config=config
-    )
+    #npi_seir = seir.build_npi_SEIR(
+    #    inference_simulator.s, load_ID=False, sim_id2load=None, config=config
+    #)
+
+    inference_simulator.s.write_simID(ftype="hnpi", sim_id=1, df=npi_outcomes.getReductionDF())
 
     hnpi_read = pq.read_table(
         f"{config_path_prefix}model_output/hnpi/000000001.105.hnpi.parquet"
@@ -62,9 +69,28 @@ def test_full_npis_read_write():
     import random
 
     random.seed(10)
-    outcomes.onerun_delayframe_outcomes(
-        sim_id2write=1, s=inference_simulator.s, load_ID=True, sim_id2load=1
+
+    inference_simulator = gempyor.InferenceSimulator(
+        config_path=f"{config_path_prefix}config_npi.yml",
+        run_id=105,
+        prefix="",
+        first_sim_index=1,
+        deathrate="med",
+        scenario="inference",
+        stoch_traj_flag=False,
+        out_run_id=106,
     )
+    #shutil.move('model_output/seir/000000001.105.seir.parquet', 'model_output/seir/000000001.106.seir.parquet')
+
+    #outcomes.onerun_delayframe_outcomes(
+    #    sim_id2write=1, s=inference_simulator.s, load_ID=True, sim_id2load=1
+    #)
+
+    npi_outcomes = outcomes.build_npi_Outcomes(
+        inference_simulator.s, load_ID=True, sim_id2load=1, config=config
+    )
+    inference_simulator.s.write_simID(ftype="hnpi", sim_id=1, df=npi_outcomes.getReductionDF())
+
 
     hnpi_read = pq.read_table(
         f"{config_path_prefix}model_output/hnpi/000000001.105.hnpi.parquet"
@@ -80,15 +106,26 @@ def test_full_npis_read_write():
         run_id=106,
         prefix="",
         first_sim_index=1,
-        deathrate="high_death_rate",
+        deathrate="med",
         stoch_traj_flag=False,
         out_run_id=107,
     )
+    #shutil.move('model_output/seir/000000001.106.seir.parquet', 'model_output/seir/000000001.107.seir.parquet')
 
-    assert (hpar_read == hpar_wrote).all().all()
+    #outcomes.onerun_delayframe_outcomes(
+    #    sim_id2write=1, s=inference_simulator.s, load_ID=True, sim_id2load=1
+    #)
+
+    npi_outcomes = outcomes.build_npi_Outcomes(
+        inference_simulator.s, load_ID=True, sim_id2load=1, config=config
+    )
+    inference_simulator.s.write_simID(ftype="hnpi", sim_id=1, df=npi_outcomes.getReductionDF())
+
+
     hnpi_read = pq.read_table(
         f"{config_path_prefix}model_output/hnpi/000000001.106.hnpi.parquet"
     ).to_pandas()
     hnpi_wrote = pq.read_table(
         f"{config_path_prefix}model_output/hnpi/000000001.107.hnpi.parquet"
     ).to_pandas()
+    assert (hnpi_read == hnpi_wrote).all().all()
