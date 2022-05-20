38c7ac615b382ec554dd98e21ccd65765e820402
jkamins7 <jkaminsky@jhu.edu>
2022-05-20 14:27:36 -0400

    Remove restore command

--- local_install.R
+++ local_install.R
@@ -1,5 +1,4 @@
 # Installs the custom-made packages in this repository
-renv::restore()
 library(devtools)
 
 pkg.dir <- paste0(dirname(script.name), "/R/pkgs/") # find the directory that this file is within
