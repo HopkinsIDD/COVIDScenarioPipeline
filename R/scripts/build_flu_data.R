c2b7151e3e891525b657088884e717493b440b6c
Shaun Truelove <satruelove@gmail.com>
2022-10-16 23:53:37 -0400

    adding variant proportions to build_flu_data script

--- R/scripts/build_flu_data.R
+++ R/scripts/build_flu_data.R
@@ -5,6 +5,8 @@
 library(dplyr)
 library(tidyr)
 library(tidycensus)
+library(readr)
+library(lubridate)
 
 
 option_list = list(
@@ -72,15 +74,82 @@ write_csv(us_data, config$filtering$data_path)
 
 
 
-# APPLY VARIANTS ----------------------------------------------------------
+
+
+
+
+# PULL VARIANT DATA -------------------------------------------------------
 
 variant_props_file <- config$seeding$variant_filename
 adjust_for_variant <- !is.null(variant_props_file)
 
+if (adjust_for_variant){
+    
+    # Variant Data (need to automate this data pull still)
+    variant_data <- read_csv(file.path(config$spatial_setup$base_path, "variant/WHO_NREVSS_Clinical_Labs.csv"), skip = 1)
+    
+    # location data
+    loc_data <- read_csv("data-locations/locations.csv")
+    
+    
+    # CLEAN DATA
+    
+    variant_data <- variant_data %>%
+        select(state = REGION, 
+               week = WEEK,
+               year = YEAR,
+               FluA = `TOTAL A`,
+               FluB = `TOTAL B`) %>%
+        pivot_longer(cols = starts_with("Flu"),
+                     names_to = "variant",
+                     values_to = "n") %>%
+        mutate(n = ifelse(n == "X", 0, n)) %>%
+        mutate(n = as.integer(n)) %>%
+        group_by(state, week, year) %>%
+        mutate(prop = n / sum(n, na.rm=TRUE)) %>%
+        mutate(prop_tot = sum(prop, na.rm=TRUE)) %>%
+        ungroup() %>%
+        mutate(prop = ifelse(prop_tot==0 & variant=="FluA", 1, prop)) %>%
+        group_by(state, week, year) %>%
+        mutate(prop_tot = sum(prop, na.rm=TRUE)) %>%
+        mutate(prop = prop / sum(prop, na.rm = TRUE)) %>%
+        ungroup() %>%
+        select(-prop_tot, -n) %>%
+        mutate(prop = ifelse(is.na(prop), 0, prop)) %>%
+        filter(!is.na(week)) %>%
+        mutate(week_end = as_date(MMWRweek::MMWRweek2Date(year, week))+1)
+    
+    match_data <- loc_data %>% 
+        select(state = location_name,
+               source = abbreviation) %>%
+        expand_grid(week_end = seq.Date(min(variant_data$week_end), max(variant_data$week_end), 7),
+                    variant = c("FluA","FluB")) %>%
+        filter(state != "US")
+    
+    variant_data <- variant_data %>%
+        full_join(match_data) %>%
+        mutate(week = epiweek(week_end), year = epiyear(week_end)) %>%
+        mutate(prop = ifelse(is.na(prop) & variant=="FluA", 1, prop)) %>%
+        mutate(prop = ifelse(is.na(prop) & variant!="FluA", 0, prop))
+    
+    variant_data <- variant_data %>%
+        expand_grid(day = 1:7) %>%
+        mutate(date = as_date(MMWRweek::MMWRweek2Date(year, week, day))) %>%
+        select(-c(week, year, day, week_end, state))
+    
+    write_csv(variant_data, variant_props_file)
+}
+
+
+# APPLY VARIANTS ----------------------------------------------------------
+
+
 if (adjust_for_variant) {
+    
+    us_data <- read_csv(config$filtering$data_path)
+    
     tryCatch({
         us_data <- covidcommon::do_variant_adjustment(us_data, variant_props_file)
-        us_data <- us_data %>% select(-prop_tot)
         write_csv(us_data, config$filtering$data_path)
     }, error = function(e) {
         stop(paste0("Could not use variant file |", variant_props_file, 
