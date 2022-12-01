31ba5e0572b39acd22bd16a30665cee0c7453a7f
Shaun Truelove <satruelove@gmail.com>
2022-12-01 14:08:47 -0500

    fixed variant

--- R/scripts/build_flu_data.R
+++ R/scripts/build_flu_data.R
@@ -96,7 +96,7 @@ adjust_for_variant <- !is.null(variant_props_file)
 if (adjust_for_variant){
     
     # Variant Data (need to automate this data pull still)
-    # variant_data <- read_csv(file.path(config$spatial_setup$base_path, "variant/WHO_NREVSS_Clinical_Labs.csv"), skip = 1)
+    #variant_data <- read_csv(file.path(config$spatial_setup$base_path, "variant/WHO_NREVSS_Clinical_Labs.csv"), skip = 1)
     variant_data <- cdcfluview::who_nrevss(region="state", years = 2022)$clinical_labs
     
     # location data
@@ -111,65 +111,61 @@ if (adjust_for_variant){
                year = year,
                FluA = total_a,
                FluB = total_b) %>%
-      # select(state = REGION, 
-      #        week = WEEK,
-      #        year = YEAR,
-      #        FluA = `TOTAL A`,
-      #        FluB = `TOTAL B`) %>%
+        # select(state = REGION, 
+        #        week = WEEK,
+        #        year = YEAR,
+        #        FluA = `TOTAL A`,
+        #        FluB = `TOTAL B`) %>%
         pivot_longer(cols = starts_with("Flu"),
                      names_to = "variant",
                      values_to = "n") %>%
         # mutate(n = ifelse(n == "X", 0, n)) %>%
         mutate(n = ifelse(is.na(n), 0, n)) %>%
         mutate(n = as.integer(n)) %>%
-        group_by(state, week, year) %>%
+        mutate(week_end = as_date(MMWRweek::MMWRweek2Date(year, week, 7))) %>%
+        full_join(expand_grid(variant = c("FluA", "FluB"),
+                              state = unique(loc_data %>% filter(location!="US") %>% pull(location_name)),
+                              week_end = as_date(seq(lubridate::as_date(config$start_date)+6, lubridate::as_date(config$end_date_groundtruth), 7)))) %>%
+        arrange(state, week_end) %>%
+        select(-week, -year) %>%
+        group_by(state, week_end) %>%
         mutate(prop = n / sum(n, na.rm=TRUE)) %>%
         mutate(prop_tot = sum(prop, na.rm=TRUE)) %>%
         ungroup() %>%
         mutate(prop = ifelse(prop_tot==0 & variant=="FluA", 1, prop)) %>%
-        group_by(state, week, year) %>%
+        group_by(state, week_end) %>%
         mutate(prop_tot = sum(prop, na.rm=TRUE)) %>%
         mutate(prop = prop / sum(prop, na.rm = TRUE)) %>%
         ungroup() %>%
         select(-prop_tot, -n) %>%
         mutate(prop = ifelse(is.na(prop), 0, prop)) %>%
-        filter(!is.na(week)) %>%
-        mutate(week_end = as_date(MMWRweek::MMWRweek2Date(year, week, 7))) %>%
+        filter(!is.na(week_end)) %>%
         filter(week_end <= as_date(end_date_))
     
-    match_data <- loc_data %>% 
-        select(state = location_name,
-               source = abbreviation) %>%
-        expand_grid(week_end = seq.Date(min(variant_data$week_end), max(variant_data$week_end), 7),
-                    variant = c("FluA","FluB")) %>%
-        filter(state != "US")
-    
     variant_data <- variant_data %>%
-        full_join(match_data) %>%
-        mutate(week = epiweek(week_end), year = epiyear(week_end)) %>%
-        mutate(prop = ifelse(is.na(prop) & variant=="FluA", 1, prop)) %>%
-        mutate(prop = ifelse(is.na(prop) & variant!="FluA", 0, prop))
+        left_join(loc_data %>% select(state = location_name, source = abbreviation)) %>%
+        mutate(week = epiweek(week_end), year = epiyear(week_end))
     
     if(end_date_ != max(variant_data$week_end)){
-      # Extend to dates of groundtruth
-      var_max_dates <- variant_data %>% 
-        group_by(source, state) %>%
-        filter(week_end == max(week_end)) %>%
-        ungroup() %>%
-        mutate(max_date = as_date(end_date_)) %>%
-        mutate(weeks_missing = as.integer(max_date - week_end)/7) %>%
-        rowwise() %>%
-        mutate(weeks_missing = paste((seq(from = 1, to=weeks_missing, 1)*7 + week_end), collapse = ",")) %>%
-        # mutate(weeks_missing = list(as_date(seq(from = 1, to=weeks_missing, 1)*7 + week_end))) #%>%
-        ungroup()
-      var_max_dates <- var_max_dates %>%
-        rename(max_current = week_end) %>%
-        mutate(week_end = strsplit(as.character(weeks_missing), ",")) %>% 
-        unnest(week_end) %>%
-        select(state, week, year, variant, prop, week_end, source) %>%
-        mutate(week_end = as_date(week_end))
-      variant_data <- variant_data %>%
-        bind_rows(var_max_dates)
+        # Extend to dates of groundtruth
+        var_max_dates <- variant_data %>% 
+            group_by(source, state) %>%
+            filter(week_end == max(week_end)) %>%
+            ungroup() %>%
+            mutate(max_date = as_date(end_date_)) %>%
+            mutate(weeks_missing = as.integer(max_date - week_end)/7) %>%
+            rowwise() %>%
+            mutate(weeks_missing = paste((seq(from = 1, to=weeks_missing, 1)*7 + week_end), collapse = ",")) %>%
+            # mutate(weeks_missing = list(as_date(seq(from = 1, to=weeks_missing, 1)*7 + week_end))) #%>%
+            ungroup()
+        var_max_dates <- var_max_dates %>%
+            rename(max_current = week_end) %>%
+            mutate(week_end = strsplit(as.character(weeks_missing), ",")) %>% 
+            unnest(week_end) %>%
+            select(state, week, year, variant, prop, week_end, source) %>%
+            mutate(week_end = as_date(week_end))
+        variant_data <- variant_data %>%
+            bind_rows(var_max_dates)
     }
     
     variant_data <- variant_data %>%
@@ -178,13 +174,10 @@ if (adjust_for_variant){
     variant_data <- variant_data %>%
         expand_grid(day = 1:7) %>%
         mutate(date = as_date(MMWRweek::MMWRweek2Date(year, week, day))) %>%
-        select(-c(week, year, day, state))
-    
-    variant_data <- variant_data %>%
-        select(-week_end)
+        select(c(variant, prop, source, date))
     
     variant_data <- variant_data %>% 
-            filter(date >= as_date(config$start_date) & date <= as_date(config$end_date_groundtruth))
+        filter(date >= as_date(config$start_date) & date <= as_date(config$end_date_groundtruth))
     
     write_csv(variant_data, variant_props_file)
 }
@@ -207,7 +200,7 @@ if (adjust_for_variant) {
                     "|, with error message", e$message))
     })
 }
-    
+
 
 
 cat(paste0("Ground truth data saved\n", 
